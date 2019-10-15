bb78cbe5505c4d2de5f0b5e6bf1dba57b1aad708
Vimalraj Selvam <email2vimalraj@gmail.com>
2019-10-15 23:06:04 +0800

    Added created and updated timestamp fields for each table

--- data/ddl.sql
+++ data/ddl.sql
@@ -1,56 +1,79 @@
-create table users (
+create table users
+(
     id uuid primary key,
     first_name text not null ,
     last_name text,
     email text not null,
-    password_digest text
+    password_digest text,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create table social_providers (
+create table social_providers
+(
     id uuid primary key,
-    name text
+    name text,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create table social_provider_users (
+create table social_provider_users
+(
     id uuid primary key,
     provider_id uuid references social_providers(id) not null,
     provider_user_id text not null,
-    user_id uuid references users(id) not null
+    user_id uuid references users(id) not null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create table locations (
+create table locations
+(
     id uuid primary key,
     country text not null,
     city text not null,
     state text,
-    zip text
+    zip text,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create table groups (
+create table groups
+(
     id uuid primary key ,
     name text unique not null,
     description text not null,
     details jsonb,
     location_id uuid references locations(id) not null,
-    creator_id uuid references users(id) not null
+    creator_id uuid references users(id) not null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create table venues (
+create table venues
+(
     id uuid primary key,
     name text not null,
-    location_id uuid references locations(id) not null
+    location_id uuid references locations(id) not null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create type sponsor_type as enum ('FOOD', 'VENUE', 'OTHER');
+create type sponsor_type as enum
+('FOOD', 'VENUE', 'OTHER');
 
-create table sponsors (
+create table sponsors
+(
     id uuid primary key,
     name text not null,
     website text,
-    type sponsor_type not null
+    type sponsor_type not null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create table events (
+create table events
+(
     id uuid primary key,
     name text not null,
     description text,
@@ -63,23 +86,31 @@ create table events (
     capacity int not null
 );
 
-create table tags (
+create table tags
+(
     id uuid primary key,
     name text not null
 );
 
-create table event_sponsors (
+create table event_sponsors
+(
     event_id uuid references events(id),
-    sponsor_id uuid references sponsors(id)
+    sponsor_id uuid references sponsors(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create table user_groups (
+create table user_groups
+(
     user_id uuid references users(id),
     group_id uuid references groups(id),
-    primary key (user_id, group_id)
+    primary key (user_id, group_id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create table rsvps (
+create table rsvps
+(
     user_id uuid references  users(id),
     event_id uuid references events(id),
     date timestamptz not null,
@@ -87,8 +118,11 @@ create table rsvps (
     primary key (user_id, event_id)
 );
 
-create table user_bans (
+create table user_bans
+(
     user_id uuid references users(id),
     group_id uuid references groups(id),
-    primary key (user_id, group_id)
+    primary key (user_id, group_id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );

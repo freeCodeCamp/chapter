98d6cbe1f3cff3aa3918a074e6955d31cfc3f187
VimalRaj Selvam <email2vimalraj@gmail.com>
2019-10-18 08:34:03 +0800

    Added created and updated timestamp fields for each table (#31)
    * Added created and updated timestamp fields for each table
    
    * Made all the keywords lowercase

--- data/ddl.sql
+++ data/ddl.sql
@@ -1,94 +1,132 @@
-create table users (
-    id uuid primary key,
-    first_name text not null ,
-    last_name text,
-    email text not null,
-    password_digest text
+create table users
+(
+  id uuid primary key,
+  first_name text not null ,
+  last_name text,
+  email text not null,
+  password_digest text,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create table social_providers (
-    id uuid primary key,
-    name text
+create table social_providers
+(
+  id uuid primary key,
+  name text,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create table social_provider_users (
-    id uuid primary key,
-    provider_id uuid references social_providers(id) not null,
-    provider_user_id text not null,
-    user_id uuid references users(id) not null
+create table social_provider_users
+(
+  id uuid primary key,
+  provider_id uuid references social_providers(id) not null,
+  provider_user_id text not null,
+  user_id uuid references users(id) not null,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create table locations (
-    id uuid primary key,
-    country text not null,
-    city text not null,
-    state text,
-    postal_code text
+create table locations
+(
+  id uuid primary key,
+  country text not null,
+  city text not null,
+  state text,
+  postal_code text,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create table chapters (
-    id uuid primary key ,
-    name text unique not null,
-    description text not null,
-    details jsonb,
-    location_id uuid references locations(id) not null,
-    creator_id uuid references users(id) not null
+create table chapters
+(
+  id uuid primary key ,
+  name text unique not null,
+  description text not null,
+  details jsonb,
+  location_id uuid references locations(id) not null,
+  creator_id uuid references users(id) not null,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create table venues (
-    id uuid primary key,
-    name text not null,
-    location_id uuid references locations(id) not null
+create table venues
+(
+  id uuid primary key,
+  name text not null,
+  location_id uuid references locations(id) not null,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create type sponsor_type as enum ('FOOD', 'VENUE', 'OTHER');
+create type sponsor_type as enum
+('FOOD', 'VENUE', 'OTHER');
 
-create table sponsors (
-    id uuid primary key,
-    name text not null,
-    website text,
-    type sponsor_type not null
+create table sponsors
+(
+  id uuid primary key,
+  name text not null,
+  website text,
+  type sponsor_type not null,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create table tags (
-    id uuid primary key,
-    name text not null
+create table events
+(
+  id uuid primary key,
+  name text not null,
+  description text,
+  starts_at timestamptz not null,
+  ends_at timestamptz not null,
+  chapter_id uuid references chapters(id) not null ,
+  venue_id uuid references venues(id),
+  tag_id uuid references tags(id),
+  canceled boolean default false,
+  capacity int not null,
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create table events (
-    id uuid primary key,
-    name text not null,
-    description text,
-    starts_at timestamptz not null,
-    ends_at timestamptz not null,
-    chapter_id uuid references chapters(id) not null ,
-    venue_id uuid references venues(id),
-    tag_id uuid references tags(id),
-    canceled boolean default false,
-    capacity int not null
+create table tags
+(
+  id uuid primary key,
+  name text not null
 );
 
-create table event_sponsors (
-    event_id uuid references events(id),
-    sponsor_id uuid references sponsors(id)
+create table event_sponsors
+(
+  event_id uuid references events(id),
+  sponsor_id uuid references sponsors(id),
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create table user_chapters (
-    user_id uuid references users(id),
-    chapter_id uuid references chapters(id),
-    primary key (user_id, chapter_id)
+create table user_chapters
+(
+  user_id uuid references users(id),
+  chapter_id uuid references chapters(id),
+  primary key(user_id, chapter_id),
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create table rsvps (
-    user_id uuid references  users(id),
-    event_id uuid references events(id),
-    date timestamptz not null,
-    on_waitlist boolean not null default FALSE,
-    primary key (user_id, event_id)
+create table rsvps
+(
+  user_id uuid references  users(id),
+  event_id uuid references events(id),
+  date timestamptz not null,
+  on_waitlist boolean not null default FALSE,
+  primary key(user_id, event_id),
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );
 
-create table user_bans (
-    user_id uuid references users(id),
-    chapter_id uuid references chapters(id),
-    primary key (user_id, chapter_id)
+create table user_bans
+(
+  user_id uuid references users(id),
+  chapter_id uuid references chapters(id),
+  primary key(user_id, chapter_id),
+  created_at timestamp default CURRENT_TIMESTAMP,
+  updated_at timestamp default CURRENT_TIMESTAMP
 );

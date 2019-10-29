eadc2800ab49f1014e70a22430f9c13175680272
Thomas Roest <ThomasRoest@users.noreply.github.com>
2019-10-29 18:20:56 +0100

    feat: add material UI framework (#154)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,27 @@
 import App from 'next/app';
+import Head from 'next/head';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
+import { CssBaseline } from '@material-ui/core';
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
+        <Head>
+          <title>Chapter</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
     );

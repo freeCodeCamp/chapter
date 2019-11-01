3653444075cefbb9bd56b62fdd474839ff0bfea7
Nikhil John <nikhil.john.88@gmail.com>
2019-10-31 22:00:21 -0600

    Fix breaking test

--- client/components/__test__/SomeComponent.test.tsx
+++ client/components/__test__/SomeComponent.test.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-
+import { ThemeProvider } from 'styled-components';
 import { SomeComponent } from 'client/components/';
+import { themeObject } from 'styles/theme';
 
 describe('SomeComponent', () => {
   it('should render text', () => {
-    const { getByText } = render(<SomeComponent />);
-    expect(
-      getByText('This is an imported component with a Button'),
-    ).toBeDefined();
+    const { container } = render(
+      <ThemeProvider theme={themeObject}>
+        <SomeComponent />
+      </ThemeProvider>,
+    );
+    expect(container.firstChild).toMatchSnapshot();
   });
 });

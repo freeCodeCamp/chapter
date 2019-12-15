147c0dee4852c5df8b3ecac1f1bc0bd53a618395
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2019-12-15 21:31:48 +0530

    Fix: CI issues

--- server/factories/chapter.factory.ts
+++ server/factories/chapter.factory.ts
@@ -1,6 +1,8 @@
 import Faker from 'faker';
 import { define } from 'typeorm-seeding';
 import { Chapter } from '../models/Chapter';
+import { Location } from '../models/Location';
+import { User } from '../models/User';
 
 define(Chapter, (faker: typeof Faker) => {
   const name = faker.company.companyName();
@@ -9,12 +11,26 @@ define(Chapter, (faker: typeof Faker) => {
   const details = {
     email: faker.internet.email(),
   };
+  const location = new Location({
+    country_code: faker.address.countryCode(),
+    city: faker.address.city(),
+    region: faker.address.state(),
+    postal_code: faker.address.zipCode(),
+  });
+  const creator = new User({
+    first_name: faker.name.firstName(),
+    last_name: faker.name.lastName(),
+    email: faker.internet.email(),
+    password_digest: faker.internet.password(),
+  });
 
   const chapter = new Chapter({
     name,
     description,
     category,
     details,
+    location,
+    creator,
   });
   return chapter;
 });

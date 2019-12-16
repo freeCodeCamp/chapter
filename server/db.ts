f95fba6eb2c4c45b3109b6358ea0e1b305972045
Aryan J <aryanjabbari@gmail.com>
2019-12-16 08:21:21 -0500

    Add migrations to db.ts (#294)
    * Add migrations to db.ts
    
    * Use ormconfig.json to populate connection options

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,18 @@
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
+import {
+  createConnection,
+  getConnectionOptions,
+  ConnectionOptions,
+} from 'typeorm';
 
-export const initDB = createConnection({
-  type: 'postgres',
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'chapter',
-  host: process.env.DB_URL || 'db',
-  port: 5432,
-  entities: [__dirname + '/models/*.ts'],
-  synchronize: false,
-  logging: false,
-  name: 'default',
-});
+export const initDB = (async () => {
+  const connectionOptions = await getConnectionOptions();
+  return createConnection({
+    ...connectionOptions,
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'chapter',
+    host: process.env.DB_URL || 'db',
+    port: 5432,
+  } as ConnectionOptions);
+})();

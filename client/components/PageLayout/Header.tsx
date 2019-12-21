a4097c268686a202e027e966d9eec70119606551
Aryan Jabbari <Aryan@applecart.co>
2019-12-21 11:23:46 -0500

    Populate Header with config headerLinks

--- client/components/PageLayout/Header.tsx
+++ client/components/PageLayout/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { headerLinks } from '../../config';
 
 const Header = ({ classes }) => (
   <header className={`${classes.pageRoot} ${classes.headerContainer}`}>
@@ -6,10 +7,11 @@ const Header = ({ classes }) => (
     <img src="/freecodecamp-logo.svg" alt="The freeCodeCamp logo" />
     <nav>
       <ul className={classes.navList}>
-        <li className={classes.navListItem}>Events</li>
-        <li className={classes.navListItem}>Chapters</li>
-        <li className={classes.navListItem}>Login</li>
-        <li className={classes.navListItem}>Register</li>
+        {headerLinks.map(headerLink => (
+          <li className={classes.navListItem} key={headerLink.name}>
+            {headerLink.label}
+          </li>
+        ))}
       </ul>
     </nav>
   </header>
---
+++ client/config.ts
@@ -0,0 +1,6 @@
+export const headerLinks: { name: string; label: string; href: string }[] = [
+  { name: 'events', label: 'Events', href: '/events' },
+  { name: 'chapters', label: 'Chapters', href: '/chapters' },
+  { name: 'login', label: 'Login', href: '/login' },
+  { name: 'register', label: 'Register', href: '/register' },
+];

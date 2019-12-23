c388e7dd3d80c9af1a32d269bb7782494ec346bf
Aryan Jabbari <Aryan@applecart.co>
2019-12-23 12:38:00 -0500

    Use Material-UI grid system

--- client/components/PageLayout/Header.tsx
+++ client/components/PageLayout/Header.tsx
@@ -1,19 +1,34 @@
+import Grid from '@material-ui/core/Grid';
 import React from 'react';
 import { headerLinks } from '../../config';
 
 const Header = ({ classes }) => (
-  <header className={`${classes.pageRoot} ${classes.headerContainer}`}>
-    <div>Search Bar here</div>
-    <img src="/freecodecamp-logo.svg" alt="The freeCodeCamp logo" />
-    <nav>
-      <ul className={classes.navList}>
+  <Grid
+    className={`${classes.pageRoot} ${classes.headerContainer}`}
+    container
+    component="header"
+    alignItems="center"
+  >
+    <Grid item xs={12} md={4}>
+      Search Bar here
+    </Grid>
+    <Grid
+      item
+      component="img"
+      src="/freecodecamp-logo.svg"
+      alt="The freeCodeCamp logo"
+      xs={12}
+      md={4}
+    />
+    <Grid component="nav" item xs={12} md={4}>
+      <Grid container direction="row" spacing={2} justify="center">
         {headerLinks.map(headerLink => (
-          <li className={classes.navListItem} key={headerLink.name}>
+          <Grid item key={headerLink.name}>
             {headerLink.label}
-          </li>
+          </Grid>
         ))}
-      </ul>
-    </nav>
-  </header>
+      </Grid>
+    </Grid>
+  </Grid>
 );
 export default Header;
--- client/components/PageLayout/pageLayout.styles.ts
+++ client/components/PageLayout/pageLayout.styles.ts
@@ -1,30 +1,6 @@
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-  headerContainer: {
-    'align-items': 'center',
-    backgroundColor: theme.palette.secondary.main,
-    color: 'white',
-    display: 'grid',
-    'justify-items': 'center',
-    [theme.breakpoints.down('sm')]: {
-      'grid-template-rows': '1fr 1fr 1fr',
-    },
-    [theme.breakpoints.down('md')]: {
-      'grid-template-columns': 'none',
-    },
-    [theme.breakpoints.up('md')]: {
-      'grid-template-columns': '1fr 1fr 1fr',
-    },
-  },
-  navList: {
-    display: 'grid',
-    'list-style': 'none',
-    'grid-column-gap': theme.spacing(1),
-    'grid-template-columns': '1fr 1fr 1fr 1fr',
-    'text-align': 'center',
-    'padding-inline-start': 0,
-  },
   pageRoot: {
     [theme.breakpoints.down('xl')]: {
       padding: `0 ${theme.spacing(8)}px`,
@@ -34,6 +10,16 @@ const useStyles = makeStyles(theme => ({
     },
     [theme.breakpoints.down('sm')]: {
       padding: `0 ${theme.spacing(2)}px`,
+    },
+  },
+  headerContainer: {
+    backgroundColor: theme.palette.secondary.main,
+    color: 'white',
+    'text-align': 'center',
+    'padding-bottom': `${theme.spacing(2)}px`,
+    'padding-top': `${theme.spacing(2)}px`,
+    [theme.breakpoints.up('md')]: {
+      'justify-content': 'space-between',
     },
   },
 }));

758be3ccb6403d5c44814e2aa858458b566b2a57
Fran Zekan <zekan.fran369@gmail.com>
2019-12-26 00:14:11 +0100

    Add simple page for showing events

---
+++ pages/dashboard/events.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { Card, Typography, Grid } from '@material-ui/core';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { ProgressCardContent } from 'client/components';
+import { AppStoreState } from 'client/store/reducers';
+import { eventActions } from 'client/store/actions';
+import { IEventModal } from 'client/store/types/events';
+
+const Events: React.FC = () => {
+  const { error, loading, events } = useSelector((state: AppStoreState) => ({
+    error: state.events.error,
+    loading: state.events.loading,
+    events: state.events.events,
+  }));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(eventActions.fetchEvents('1'));
+  }, []);
+
+  console.log(error, loading, events);
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={10}>
+          <ProgressCardContent loading={loading}>
+            <Card>
+              <button style={{ float: 'right' }}>Add new</button>
+              {error ? (
+                <h1>😢Error</h1>
+              ) : (
+                events.map((event: IEventModal) => (
+                  <div key={`event-${event.id}`}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {event.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {event.description}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {event.capacity}
+                    </Typography>
+                  </div>
+                ))
+              )}
+            </Card>
+          </ProgressCardContent>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Events;

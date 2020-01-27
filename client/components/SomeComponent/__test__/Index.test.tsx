5fee36dccfde35fccf7b45f00456b2fc59beb1ec
Fran Zekan <zekan.fran369@gmail.com>
2020-01-27 20:48:41 +0100

    Refactor example to not use styledComponents

--- client/components/SomeComponent/__test__/Index.test.tsx
+++ client/components/SomeComponent/__test__/Index.test.tsx
@@ -4,15 +4,15 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from '@material-ui/core/styles';
 import { SomeComponent } from 'client/components';
-import { themeObject } from 'styles/theme';
+import theme from 'styles/theme';
 
 describe('SomeComponent', () => {
   it('should render text', () => {
     const { container } = render(
-      <ThemeProvider theme={themeObject}>
-        <SomeComponent />
+      <ThemeProvider theme={theme}>
+        <SomeComponent />,
       </ThemeProvider>,
     );
     expect(container.firstChild).toMatchSnapshot();

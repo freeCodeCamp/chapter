f5ecf112010e555c86ff6faa567da5e0218478dc
Josh Ghent <josh.ghent@capp.co>
2020-02-06 16:47:01 +0000

    Added adding of location and user to the creation process of the chapter

--- server/routes/tests/chapterRouter.test.ts
+++ server/routes/tests/chapterRouter.test.ts
@@ -1,5 +1,6 @@
 import App from 'server/testUtils/App';
 import chapterRouter from 'server/routes/v1/chapter';
+import { User } from 'server/models';
 
 describe('router: sampleRouter', () => {
   const app = new App();
@@ -8,28 +9,44 @@ describe('router: sampleRouter', () => {
     await app.initialize({ withRouter: chapterRouter });
   });
 
-  describe('/api/v1/chapter', () => {
+  describe('/chapter', () => {
     it('Creates a Chapter', async () => {
-      const res = await app.request.post('/api/v1/chapter/1').send({
+      const locations = await app.request.get('/locations');
+
+      const user = await app.request.post('/user').send({
+        first_name: 'Test',
+        last_name: 'Agent',
+        email: 'example@freecodecamp.com',
+      });
+
+      const res = await app.request.post('/chapters').send({
         name: 'Test',
         description: 'Test',
         category: 'Test',
         details: 'Hello World',
-        location: 'Birmingham, UK',
-        creator: 1,
+        location: locations[0].id,
+        creator: user.id,
       });
 
+      expect(res.error).toBe(undefined);
+      expect(res.body).toStrictEqual({
+        name: 'Test',
+        description: 'Test',
+        category: 'Test',
+        details: 'Hello World',
+        creator: 1,
+      });
       expect(res.status).toBe(201);
     });
 
     it('Adds a user to the chapter', async () => {
-      const res = await app.request.get('/api/v1/chapter/1/join/1');
+      const res = await app.request.get('/chapters/1/join/1');
 
       expect(res.status).toBe(201);
     });
 
     it('Bans a user from a chapter', async () => {
-      const res = await app.request.get('/api/v1/chapter/1/ban/1');
+      const res = await app.request.get('/chapters/1/ban/1');
 
       expect(res.status).toBe(201);
     });

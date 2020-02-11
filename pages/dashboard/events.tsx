5e2cabfcbbb9483502c7490c4717419ec50c6496
Fran Zekan <zekan.fran369@gmail.com>
2020-02-11 22:21:38 +0100

    Extract DashboarEvent into a separate component

--- pages/dashboard/events.tsx
+++ pages/dashboard/events.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
-import { Card, Typography, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { ProgressCardContent } from 'client/components';
 import { AppStoreState } from 'client/store/reducers';
 import { eventActions } from 'client/store/actions';
 import { IEventModal } from 'client/store/types/events';
+import DashboardEvent from 'client/components/DashboardEvent';
 
 const Events: React.FC = () => {
   const { error, loading, events } = useSelector((state: AppStoreState) => ({
@@ -28,27 +28,7 @@ const Events: React.FC = () => {
             <h1>😢Error</h1>
           ) : (
             events.map((event: IEventModal) => (
-              <Card style={{ marginTop: '12px' }} key={`event-${event.id}`}>
-                <ProgressCardContent loading={loading}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {event.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {event.description}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {event.capacity}
-                  </Typography>
-                </ProgressCardContent>
-              </Card>
+              <DashboardEvent event={event} loading={loading} key={event.id} />
             ))
           )}
         </Grid>

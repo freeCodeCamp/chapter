3b89d440ba75dca97f93e42a17755cc6b85d2735
Fran Zekan <zekan.fran369@gmail.com>
2020-02-13 15:38:17 +0100

    WIP: add create page\

---
+++ pages/dashboard/events/create.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { FormControl, TextField, Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '25%',
+  },
+  item: {
+    marginTop: '20px',
+  },
+}));
+
+const CreateEvent: React.FC = () => {
+  const { control, handleSubmit } = useForm();
+  const styles = useStyles();
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <FormControl className={styles.item}>
+          <Controller
+            control={control}
+            as={
+              <TextField
+                label="Event title"
+                type="text"
+                placeholder="Foo and the bars"
+              />
+            }
+            name="name"
+            defaultValue=""
+            options={{ required: true }}
+          />
+        </FormControl>
+        <FormControl className={styles.item}>
+          <Controller
+            control={control}
+            as={<TextField label="Capacity" type="number" placeholder="50" />}
+            name="capacity"
+            defaultValue="50"
+            options={{ required: true }}
+          />
+        </FormControl>
+        <FormControl className={styles.item}>
+          <Controller
+            control={control}
+            as={
+              <TextField
+                label="Tags (separated by a comma)"
+                type="text"
+                placeholder="Foo, bar"
+              />
+            }
+            name="tags"
+            defaultValue=""
+            options={{ required: true }}
+          />
+        </FormControl>
+        <Button
+          className={styles.item}
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          Add Event
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default CreateEvent;

609ac8044ac3102907fa9ba671df0fc6e38d65ad
timmyichen <timmy.i.chen@gmail.com>
2020-02-13 09:08:45 +0000

    add role tables

---
+++ server/models/UserChapterRole.ts
@@ -0,0 +1,37 @@
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { User } from './User';
+import { Chapter } from './Chapter';
+
+export type ChapterRoles = 'organizer' | 'member';
+
+@Entity({ name: 'user_chapter_roles' })
+export class UserChapterRole {
+  @PrimaryColumn()
+  user_id!: number;
+
+  @PrimaryColumn()
+  chapter_id!: number;
+
+  @PrimaryColumn()
+  role_name!: ChapterRoles;
+
+  @ManyToOne(_type => User)
+  @JoinColumn({ name: 'user_id' })
+  user!: User;
+
+  @ManyToOne(_type => User)
+  @JoinColumn({ name: 'chapter_id' })
+  chapter!: Chapter;
+
+  constructor(params: {
+    userId: number;
+    roleName: ChapterRoles;
+    chapterId: number;
+  }) {
+    if (params) {
+      this.user_id = params.userId;
+      this.role_name = params.roleName;
+      this.chapter_id = params.chapterId;
+    }
+  }
+}

ad073c1c80e84832104cdfaf644ccd19af1d8b0b
Fran Zekan <zekan.fran369@gmail.com>
2020-02-13 20:19:30 +0100

    Load ormconfig from dotenv

---
+++ ormconfig.js
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { config } = require('dotenv');
+
+config();
+
+const dbConfig = {
+  host: process.env.DB_URL,
+  port: process.env.DB_PORT || (process.env.DB_LOCAL !== '' ? 5432 : 54320),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+// TODO: remove debue
+console.log({ port: process.env.DB_PORT, local: process.env.DB_LOCAL });
+console.log(dbConfig);
+
+module.exports = {
+  type: 'postgres',
+  ...dbConfig,
+  synchronize: false,
+  logging: false,
+  entities: ['server/models/**/*.ts'],
+  factories: ['server/factories/**/*.factory.ts'],
+  migrations: ['server/migrations/**/*.ts'],
+  seeds: ['server/seeds/**/*.seed.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'server/models',
+    migrationsDir: 'server/migrations',
+    subscribersDir: 'server/subscriber',
+  },
+};

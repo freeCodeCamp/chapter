bde1aa490fc0fb019a5c126b3369a581fb550e71
Fran Zekan <zekan.fran369@gmail.com>
2020-02-13 20:22:19 +0100

    Add tmp script for reading env variables and tesing

---
+++ readENV.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { config } = require('dotenv');
+
+config();
+
+const entries = Object.keys(process.env)
+  .filter(
+    item =>
+      item.startsWith('DB') ||
+      item.startsWith('EMAIL') ||
+      item.startsWith('CHAPTER'),
+  )
+  .map(item => [item, process.env[item]]);
+
+const env = entries.reduce((obj, [key, value]) => {
+  obj[key] = value;
+  return obj;
+}, {});
+
+console.log(env);

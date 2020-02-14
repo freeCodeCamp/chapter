1ce1506e85016afc9d54fc8b8203d2da92b8881f
Tim Chen <timmyichen@users.noreply.github.com>
2020-02-14 07:26:50 -0500

    feat(models): add role tables (#335)

---
+++ server/models/UserInstanceRole.ts
@@ -0,0 +1,24 @@
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { User } from './User';
+
+export type InstanceRoles = 'admin' | 'owner';
+
+@Entity({ name: 'user_instance_roles' })
+export class UserInstanceRole {
+  @PrimaryColumn()
+  user_id!: number;
+
+  @PrimaryColumn()
+  role_name!: InstanceRoles;
+
+  @ManyToOne(_type => User)
+  @JoinColumn({ name: 'user_id' })
+  user!: User;
+
+  constructor(params: { userId: number; roleName: InstanceRoles }) {
+    if (params) {
+      this.user_id = params.userId;
+      this.role_name = params.roleName;
+    }
+  }
+}

6a9e1e6895d2163812adc9ca40869d8fff6d9761
Fran Zekan <zekan.fran369@gmail.com>
2020-02-16 01:12:26 +0100

    Update UI to add cancel event button

--- client/components/DashboardEvent.tsx
+++ client/components/DashboardEvent.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Card, Typography } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Card, Typography, Button } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import Link from 'next/link';
 
 import ProgressCardContent from './ProgressCardContent';
 import { IEventModal } from 'client/store/types/events';
-import Link from 'next/link';
+import { eventActions } from 'client/store/actions';
 
 interface IDashboardEventProps {
   event: IEventModal;
@@ -11,6 +13,25 @@ interface IDashboardEventProps {
 }
 
 const DashboardEvent: React.FC<IDashboardEventProps> = ({ event, loading }) => {
+  const [confirm, setConfirm] = useState<boolean>(false);
+  const dispatch = useDispatch();
+
+  const cancel = () => {
+    if (confirm) {
+      dispatch(eventActions.cancelEvent(1, event.id));
+    } else {
+      setConfirm(true);
+    }
+  };
+
+  useEffect(() => {
+    if (confirm) {
+      setTimeout(() => {
+        setConfirm(false);
+      }, 2000);
+    }
+  }, [confirm]);
+
   return (
     <Card style={{ marginTop: '12px' }}>
       <ProgressCardContent loading={loading}>
@@ -24,12 +45,22 @@ const DashboardEvent: React.FC<IDashboardEventProps> = ({ event, loading }) => {
             </Typography>
           </a>
         </Link>
+        {event.canceled && (
+          <Typography variant="h5" color="error">
+            Canceled
+          </Typography>
+        )}
         <Typography variant="body2" color="textSecondary" component="p">
           {event.description}
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
           {event.capacity}
         </Typography>
+        {!event.canceled && (
+          <Button onClick={cancel}>
+            {confirm ? 'Are you sure?' : 'Cancel'}
+          </Button>
+        )}
       </ProgressCardContent>
     </Card>
   );

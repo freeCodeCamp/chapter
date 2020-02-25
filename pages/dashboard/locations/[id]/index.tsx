7828b68b0b584df9ab78cc720fa7eadd612f4795
Fran Zekan <zekan.fran369@gmail.com>
2020-02-25 14:09:34 +0100

    Add stub for locations show

---
+++ pages/dashboard/locations/[id]/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Card, Typography } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+import { locationActions } from 'client/store/actions';
+import { AppStoreState } from 'client/store/reducers';
+import { ProgressCardContent } from 'client/components';
+
+const ShowLocation: React.FC = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { error, loading, location } = useSelector((state: AppStoreState) => ({
+    error: state.locations.error,
+    loading: state.locations.loading,
+    location: state.locations.locations.find(
+      location => location.id === parseInt(Array.isArray(id) ? id[0] : id),
+    ),
+  }));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (id !== undefined) {
+      dispatch(locationActions.fetchOneLocation(id));
+    }
+  }, [id]);
+
+  return loading ? (
+    <h1>Loading...</h1>
+  ) : !(error || location) ? (
+    <h1>Error...</h1>
+  ) : (
+    <>
+      <Link href="/dashboard/locations">Locations</Link>
+      <Card style={{ marginTop: '12px' }} key={`event-${location.id}`}>
+        <ProgressCardContent loading={loading}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {location.address}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {`${location.region}, ${location.country_code}, ${location.postal_code}`}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {location.address}
+          </Typography>
+        </ProgressCardContent>
+      </Card>
+
+      <h3>Placeholder for venues...</h3>
+    </>
+  );
+};
+
+export default ShowLocation;

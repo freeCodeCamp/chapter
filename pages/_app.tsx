9892a519ff9184409da20e82e97ef96ae15658c4
Fran Zekan <zekan.fran369@gmail.com>
2020-02-25 14:39:39 +0100

    Reorganise imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,27 +2,13 @@ import App from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 import { Provider } from 'react-redux';
-// import { applyMiddleware, compose, createStore } from 'redux';
-// import thunk from 'redux-thunk';
 import { ThemeProvider as MaterialUIThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from 'styles/theme';
-// import { rootReducer } from 'client/store/reducers';
-import PageLayout from 'client/components/PageLayout';
-
-// const composeEnhancers =
-//   (typeof window !== 'undefined' &&
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-//   compose;
-
-// const store = createStore(
-//   rootReducer,
-//   composeEnhancers(applyMiddleware(thunk)),
-// );
-
 import withRedux from 'next-redux-wrapper';
-
 import { PersistGate } from 'redux-persist/integration/react';
+
+import theme from 'styles/theme';
+import PageLayout from 'client/components/PageLayout';
 import reduxStore from '../lib/redux';
 
 class MyApp extends App<any> {

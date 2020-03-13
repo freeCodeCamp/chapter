4f9d683471a7806223f1efe5257cdf90594abbc9
Fran Zekan <zekan.fran369@gmail.com>
2020-03-13 21:55:05 +0100

    WIP Refactor http service

--- client/services/http-service.ts
+++ client/services/http-service.ts
@@ -5,13 +5,31 @@
 
 import fetch from 'cross-fetch';
 
+type Obj = Record<string, string>;
+type HTTP_METHOD = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
 export class HttpService<V = any> {
   public static baseUrl = '/api/v1'; // TODO: need to create some ENV for this
   public static baseHeaders = {
     'Content-Type': 'application/json',
   };
 
-  public stringifyParams(params: Record<string, string>) {
+  // make it nicer on the backend for development purposes
+  private stringify(data: any): string {
+    if (process.env.NODE_ENV === 'development') {
+      return JSON.stringify(data, null, 2);
+    }
+    return JSON.stringify(data);
+  }
+
+  private urlHelper(url: string, params: Obj): string {
+    if (!url.startsWith('/')) {
+      url = `/${url}`;
+    }
+    return HttpService.baseUrl + url + this.stringifyParams(params);
+  }
+
+  private stringifyParams(params: Obj): string {
     return Object.keys(params).reduce((acc, key, i) => {
       return Object.prototype.hasOwnProperty.call(params, key)
         ? `${acc}${i !== 0 ? '&' : '?'}${key}=${params[key]}`
@@ -19,51 +37,45 @@ export class HttpService<V = any> {
     }, '');
   }
 
-  public async get(
+  private request(
     url: string,
-    params: Record<string, string>,
-    headers: Record<string, string>,
-  ) {
-    return fetch(HttpService.baseUrl + url + this.stringifyParams(params), {
-      headers: { ...HttpService.baseHeaders, ...headers },
-      method: 'GET',
-    }).then<V>(res => res.json());
-  }
-
-  public async post(
-    url: string,
-    params: Record<string, string>,
+    params: Obj,
     data: any,
-    headers: Record<string, string>,
+    headers: Obj,
+    method: HTTP_METHOD,
   ) {
-    return fetch(HttpService.baseUrl + url + this.stringifyParams(params), {
+    return fetch(this.urlHelper(url, params), {
       headers: { ...HttpService.baseHeaders, ...headers },
-      body: data,
-      method: 'POST',
-    }).then<V>(res => res.json());
+      body: this.stringify(data),
+      method,
+    });
   }
 
-  public async patch(
-    url: string,
-    params: Record<string, string>,
-    data: any,
-    headers: Record<string, string>,
-  ) {
-    return fetch(HttpService.baseUrl + url + this.stringifyParams(params), {
-      headers: { ...HttpService.baseHeaders, ...headers },
-      body: data,
-      method: 'PATCH',
-    }).then<V>(res => res.json());
+  public async get(url: string, params: Obj, headers: Obj) {
+    const res = await this.request(url, params, undefined, headers, 'GET');
+    const resData = await res.json();
+
+    return [res, resData];
   }
 
-  public async delete(
-    url: string,
-    params: Record<string, string>,
-    headers: Record<string, string>,
-  ) {
-    return fetch(HttpService.baseUrl + url + this.stringifyParams(params), {
-      headers: { ...HttpService.baseHeaders, ...headers },
-      method: 'DELETE',
-    }).then<V>(res => res.json());
+  public async post(url: string, params: Obj, data: any, headers: Obj) {
+    const res = await this.request(url, params, data, headers, 'POST');
+    const resData = await res.json();
+
+    return [res, resData];
+  }
+
+  public async patch(url: string, params: Obj, data: any, headers: Obj) {
+    const res = await this.request(url, params, data, headers, 'PATCH');
+    const resData = await res.json();
+
+    return [res, resData];
+  }
+
+  public async delete(url: string, params: Obj, headers: Obj) {
+    const res = await this.request(url, params, undefined, headers, 'DELETE');
+    const resData = await res.json();
+
+    return [res, resData];
   }
 }

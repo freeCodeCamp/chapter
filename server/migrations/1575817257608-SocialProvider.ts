2d5de092dc879cb6ff94af1b8021135b30c0af03
Fran Zekan <zekan.fran369@gmail.com>
2020-03-14 16:24:25 +0100

    Rerun prettier on migrations

--- server/migrations/1575817257608-SocialProvider.ts
+++ server/migrations/1575817257608-SocialProvider.ts
@@ -1,17 +1,17 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class SocialProvider1575817257608 implements MigrationInterface {
-  name = 'SocialProvider1575817257608';
+  name = "SocialProvider1575817257608";
 
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
       `CREATE TABLE "social_providers" (
-          "id" SERIAL NOT NULL, 
-          "name" character varying NOT NULL, 
-          "created_at" TIMESTAMP NOT NULL DEFAULT now(), 
-          "updated_at" TIMESTAMP NOT NULL DEFAULT now(), 
+          "id" SERIAL NOT NULL,
+          "name" character varying NOT NULL,
+          "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+          "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
           CONSTRAINT "PK_b436096f9efa0ae93c6dc8d1aec" PRIMARY KEY ("id"))`,
-      undefined,
+      undefined
     );
   }
 
--- server/migrations/1581805112048-AddAddressToLocation.ts
+++ server/migrations/1581805112048-AddAddressToLocation.ts
@@ -1,19 +1,13 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddAddressToLocation1581805112048 implements MigrationInterface {
-  name = 'AddAddressToLocation1581805112048';
+  name = "AddAddressToLocation1581805112048";
 
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query(
-      `ALTER TABLE "locations" ADD "address" character varying`,
-      undefined,
-    );
+    await queryRunner.query(`ALTER TABLE "locations" ADD "address" character varying`, undefined);
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query(
-      `ALTER TABLE "locations" DROP COLUMN "address"`,
-      undefined,
-    );
+    await queryRunner.query(`ALTER TABLE "locations" DROP COLUMN "address"`, undefined);
   }
 }

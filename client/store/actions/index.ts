82afa7a0eb6e538d63f07a38fa74d1da2037195a
Fran Zekan <zekan.fran369@gmail.com>
2020-03-15 16:26:08 +0100

    Feature/venues ui (#370)
    * Send location with venue
    
    * Add redux for venues
    
    * Add venue views
    
    * Add comment on useThunkDispatch hook

--- client/store/actions/index.ts
+++ client/store/actions/index.ts
@@ -1,5 +1,6 @@
 import * as chapterActions from './chapter';
 import * as eventActions from './events';
 import * as locationActions from './locations';
+import * as venueActions from './venues';
 
-export { chapterActions, eventActions, locationActions };
+export { chapterActions, eventActions, locationActions, venueActions };

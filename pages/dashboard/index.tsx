a231d39700dd5a363dc12ded1cf82d4b073d8a0d
Fran Zekan <zekan.fran369@gmail.com>
2020-03-15 17:55:01 +0100

    Nicer tmp dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import Link from 'next/link';
 
 import links from 'client/constants/DashboardLinks';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  link: {
+    fontSize: '1rem',
+    marginRight: '1rem',
+  },
+}));
 
 const Dashboard: React.FC = () => {
+  const styles = useStyles();
+
   return (
     <div>
       {links.map(item => (
         <>
           <Link href={item.link} key={item.text}>
-            <a>{item.text}</a>
+            <a className={styles.link}>{item.text}</a>
           </Link>
-          <br />
         </>
       ))}
     </div>

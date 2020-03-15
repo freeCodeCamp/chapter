835aefe856d8b639ceb1dd47e7994c87689dcf39
Fran Zekan <zekan.fran369@gmail.com>
2020-03-15 16:22:34 +0100

    Add comment on useThunkDispatch hook

--- client/hooks/useThunkDispatch.ts
+++ client/hooks/useThunkDispatch.ts
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from 'client/store/types/chapter';
 
+// Override types with our thunk setup so typescript doesn't use the default and complain
 const useThunkDispatch = () => {
   const dispatch: AppDispatch = useDispatch();
   return dispatch;

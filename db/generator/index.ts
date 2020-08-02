79bc10f3f9622651526741ef330e3281815bea1d
Fran Zekan <zekan.fran369@gmail.com>
2020-08-02 18:20:59 +0200

    Add locations seeds

--- db/generator/index.ts
+++ db/generator/index.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import createUsers from './user.factory';
+import createLocations from './locations.factory';
 
 (async () => {
   const connection = await createConnection();
 
   const [user] = await createUsers();
+  const locations = await createLocations();
 
   await connection.close();
 })();

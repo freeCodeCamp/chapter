76f57aa2325826d0a8c0b56ac2049fc5db8cb674
Fran Zekan <zekan.fran369@gmail.com>
2020-08-03 22:37:24 +0200

    Remove redux

--- client/src/store/actions/index.ts
+++
@@ -1,6 +0,0 @@
-import * as chapterActions from './chapter';
-import * as eventActions from './events';
-import * as locationActions from './locations';
-import * as venueActions from './venues';
-
-export { chapterActions, eventActions, locationActions, venueActions };

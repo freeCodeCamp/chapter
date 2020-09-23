1a072d8d5db57f916f292e59b19a27ee6da4d4fb
Paul Gamble <pgamble@gmail.com>
2020-09-23 14:04:48 -0400

    removed compile in jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+//const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest',
+  },
+  moduleDirectories: ['node_modules', 'server'],
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+  watchPathIgnorePatterns: ['/node_modules'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testMatch: ['/**/*.test.(ts|js|tsx|jsx)'],
+};

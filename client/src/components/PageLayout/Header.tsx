ce321babe5c1d7cf99b5f8c3e77606e44b7bb85d
Tim Chen <timmyichen@users.noreply.github.com>
2020-10-13 13:16:05 -0700

    Revert "Mock search bar with Material UI. refs #349 (#409)" (#414)
    This reverts commit 942f802322d4e80b1ba90e41c4754145ee33e6d7.

--- client/src/components/PageLayout/Header.tsx
+++ client/src/components/PageLayout/Header.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
 import { makeStyles, Grid } from '@material-ui/core';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
 
 import { headerLinks } from '../../constants/Header';
-import theme from '../../styles/theme';
 
 const useStyles = makeStyles({
   link: {
@@ -14,31 +11,6 @@ const useStyles = makeStyles({
   },
   cursorPointer: {
     cursor: 'pointer',
-  },
-  // c.f. https://material-ui.com/components/app-bar/#app-bar-with-search-field
-  inputInput: {
-    width: '100%',
-    color: theme.palette.secondary.main,
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '90%',
-    fontSize: '0.83rem',
-  },
-  search: {
-    position: 'relative',
-    width: '100%',
-    backgroundColor: theme.palette.background.default,
-    margin: '0.5rem 0',
-  },
-  searchIcon: {
-    height: '100%',
-    color: theme.palette.secondary.main,
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
@@ -52,6 +24,9 @@ const Header: React.FC<{ classes: Record<string, any> }> = ({ classes }) => {
       component="header"
       alignItems="center"
     >
+      <Grid item xs={12} md={4}>
+        Search Bar here
+      </Grid>
       <Link href="/">
         <Grid
           item
@@ -63,21 +38,6 @@ const Header: React.FC<{ classes: Record<string, any> }> = ({ classes }) => {
           md={4}
         />
       </Link>
-      <Grid item xs={12} md={4}>
-        <div className={styles.search}>
-          <div className={styles.searchIcon}>
-            <SearchIcon />
-          </div>
-          <InputBase
-            placeholder="Enter your city, state or country to search for events"
-            classes={{
-              root: styles.inputRoot,
-              input: styles.inputInput,
-            }}
-            inputProps={{ 'aria-label': 'search' }}
-          />
-        </div>
-      </Grid>
       <Grid component="nav" item xs={12} md={4}>
         <Grid container direction="row" spacing={2} justify="center">
           {headerLinks.map(headerLink => (

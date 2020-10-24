f32d37e3f1aaa752232ef42189c59ffed88aac29
André Jaenisch <Ryuno-Ki@users.noreply.github.com>
2020-10-24 17:54:43 +0200

    Create dummy SERP. (#410)
    * Create dummy SERP.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>
    
    * Use Typography
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>
    
    * Show headline like h4
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ client/src/modules/home/shared/components/Layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Layout: React.FC = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default Layout;
---
+++ client/src/modules/search-results/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Card, Grid, Typography } from '@material-ui/core';
+import Layout from '../home/shared/components/Layout';
+
+const SearchResults: React.FC = () => {
+  // TODO: Replace with `useEventsQuery()`!
+  // TODO: Add `key` prop to `<Card />`!
+  const data = {
+    searchResults: [],
+    searchTerm: 'London',
+  };
+
+  return (
+    <Layout>
+      <Typography variant="h4" component="h1">
+        Search results for <q>{data?.searchTerm}</q>
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={10}>
+          {data?.searchResults.length === 0 && (
+            <Typography variant="body1" component="p">
+              Nothing found.
+            </Typography>
+          )}
+          {data?.searchResults.map(searchResult => (
+            <Card>
+              <Typography gutterBottom variant="h5" component="h2">
+                {searchResult}
+              </Typography>
+            </Card>
+          ))}
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+};
+
+export default SearchResults;

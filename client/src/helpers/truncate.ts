b93888916b408c1ff0d49fab67eda450ddf237e4
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-06-03 13:21:03 +0200

    fix(client): test and update truncate
    Mostly this is to demonstrate that tests in the client directory work,
    but I noticed that truncate wasn't always doing what I expected and made
    some changes.

--- client/src/helpers/truncate.ts
+++ client/src/helpers/truncate.ts
@@ -1,4 +1,7 @@
-export const truncate = (str: string, len?: number) => {
-  const lastSpace = str.slice(0, len || 100).lastIndexOf(' ');
-  return str.slice(0, lastSpace) + '...';
+export const truncate = (str: string, len = 100) => {
+  if (str.length <= len) return str;
+  const lastSpace = str.slice(0, len - 2).lastIndexOf(' ');
+  return lastSpace === -1
+    ? str.slice(0, len - 3) + '...'
+    : str.slice(0, lastSpace) + '...';
 };

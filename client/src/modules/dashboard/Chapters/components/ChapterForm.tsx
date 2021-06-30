2003d37de4fde42c7e493416359d8709058ef860
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-06-30 10:09:37 +0200

    fix: add image to the Chapter edit form

--- client/src/modules/dashboard/Chapters/components/ChapterForm.tsx
+++ client/src/modules/dashboard/Chapters/components/ChapterForm.tsx
@@ -35,6 +35,7 @@ const fields: Fields[] = [
   ['region', true],
   ['country', true],
   ['category', true],
+  ['image', true],
 ];
 
 const ChapterForm: React.FC<ChapterFormProps> = (props) => {
@@ -49,6 +50,7 @@ const ChapterForm: React.FC<ChapterFormProps> = (props) => {
     region: chapter?.region ?? '',
     country: chapter?.country ?? '',
     category: chapter?.category ?? '',
+    image: chapter?.image ?? '',
   };
   const { control, handleSubmit } = useForm<ChapterFormData>({
     defaultValues,

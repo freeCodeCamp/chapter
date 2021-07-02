b558ed0b7a88680dac4111be3a7878518d9dc706
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-07-02 09:41:08 +0200

    feat: set npm version to 7 (Docker + CI) (#655)
    * test: set npm version to 7 for CI tests
    
    * fix: specify npm version for Docker
    
    * fix: handle postInstall in Docker
    
    * refactor: remove extra COPY command
    
    COPY . . will include the package files, so there's no need for a
    separate COPY for them.
    
    * fix: prevent husky installing in Docker

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,11 @@ FROM node:14-alpine
 
 WORKDIR /usr/chapter/
 
-# Install app dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
-# where available (npm@5+)
-COPY package*.json ./
-
-RUN npm install
-
 # Bundle app source
 COPY . .
+
+# Install app dependencies
+RUN npm i -g npm@7 && npm i
 
 EXPOSE 5000
 
--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { existsSync, copyFileSync } = require('fs');
 const { execSync } = require('child_process');
+const isDocker = require('is-docker');
 
 console.log('--------------------------');
 console.log('🎉 WELCOME TO CHAPTER 🎉');
@@ -9,45 +10,51 @@ console.log('--------------------------');
 const CHAPTER_REMOTE = 'freeCodeCamp/chapter.git';
 let IS_ERROR = false;
 
-try {
-  const rows = execSync('git remote -v', {
-    encoding: 'utf-8',
-  }).split('\n');
-
-  const isFork = rows
-    .filter((item) => item.startsWith('origin'))
-    .every((item) => !item.includes(CHAPTER_REMOTE));
-
-  const hasUpstreams = rows
-    .filter((item) => item.startsWith('upstream'))
-    .some((item) => item.includes(CHAPTER_REMOTE));
-
-  if (isFork && !hasUpstreams) {
-    console.log(
-      'It seems like this is a fork, and you dont have upstream setup.',
-    );
-    console.log('Run "scripts/pullUpstream.sh" to setup remotes.');
-  }
-} catch (e) {
-  IS_ERROR = true;
-  console.error(e);
+if (!isDocker()) {
+  setup();
 }
 
-// Copy .env.example to .env
-if (!existsSync('.env')) {
-  console.log("You don't have a .env\nCopying .env.example to .env");
-
+function setup() {
   try {
-    copyFileSync('.env.example', '.env');
-    console.log('Copied!');
+    const rows = execSync('git remote -v', {
+      encoding: 'utf-8',
+    }).split('\n');
+
+    const isFork = rows
+      .filter((item) => item.startsWith('origin'))
+      .every((item) => !item.includes(CHAPTER_REMOTE));
+
+    const hasUpstreams = rows
+      .filter((item) => item.startsWith('upstream'))
+      .some((item) => item.includes(CHAPTER_REMOTE));
+
+    if (isFork && !hasUpstreams) {
+      console.log(
+        'It seems like this is a fork, and you dont have upstream setup.',
+      );
+      console.log('Run "scripts/pullUpstream.sh" to setup remotes.');
+    }
   } catch (e) {
     IS_ERROR = true;
-    console.error(`${e} occured while copying .env file`);
+    console.error(e);
   }
-}
 
-if (!IS_ERROR) {
-  console.log(
-    '\nCongratulations, its almost done 🙌🏼. Run `npm run dev` to start the development server.',
-  );
+  // Copy .env.example to .env
+  if (!existsSync('.env')) {
+    console.log("You don't have a .env\nCopying .env.example to .env");
+
+    try {
+      copyFileSync('.env.example', '.env');
+      console.log('Copied!');
+    } catch (e) {
+      IS_ERROR = true;
+      console.error(`${e} occured while copying .env file`);
+    }
+  }
+
+  if (!IS_ERROR) {
+    console.log(
+      '\nCongratulations, its almost done 🙌🏼. Run `npm run dev` to start the development server.',
+    );
+  }
 }

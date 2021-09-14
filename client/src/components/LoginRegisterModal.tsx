105f062c0eb239388abffe7f3ff909b855944c56
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-09-14 16:17:05 +0200

    fix: handle error types

--- client/src/components/LoginRegisterModal.tsx
+++ client/src/components/LoginRegisterModal.tsx
@@ -85,7 +85,12 @@ export const LoginRegisterModal: React.FC<{
           });
       }, 1000);
     } catch (err) {
-      setError(err.message);
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('An unknown error occurred');
+        console.log(err);
+      }
     }
   };
 

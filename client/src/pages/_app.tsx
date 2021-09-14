f06a5f9b9be071bec8da71b5b4a1ed00a3bb23e6
Ravichandra <ravichandraraviraviravi@gmail.com>
2021-09-14 13:44:28 +0530

    Removed Matrial UI references from _app,_document and pageStyle/index.ts page (#704)
    Co-authored-by: Ravi <Ravichandra.cheekati@oracle.com>

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -7,8 +7,6 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { offsetLimitPagination } from '@apollo/client/utilities';
 import { ChakraProvider } from '@chakra-ui/react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider as MaterialUIThemeProvider } from '@material-ui/core/styles';
 import { ConfirmContextProvider } from 'chakra-confirm';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
@@ -16,7 +14,6 @@ import React from 'react';
 
 import PageLayout from '../components/PageLayout';
 import { AuthContextProvider } from '../modules/auth/store';
-import theme from '../styles/theme';
 
 const serverUri =
   process.env.NEXT_PUBLIC_APOLLO_SERVER || 'http://localhost:5000';
@@ -56,21 +53,17 @@ const CustomApp: React.FC<AppProps> = ({ pageProps, Component }) => {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
         />
-        <meta name="theme-color" content={theme.palette.primary.main} />
       </Head>
       <ApolloProvider client={client}>
-        <MaterialUIThemeProvider theme={theme}>
-          <CssBaseline />
-          <ChakraProvider>
-            <AuthContextProvider>
-              <ConfirmContextProvider>
-                <PageLayout>
-                  <Component {...pageProps} />
-                </PageLayout>
-              </ConfirmContextProvider>
-            </AuthContextProvider>
-          </ChakraProvider>
-        </MaterialUIThemeProvider>
+        <ChakraProvider>
+          <AuthContextProvider>
+            <ConfirmContextProvider>
+              <PageLayout>
+                <Component {...pageProps} />
+              </PageLayout>
+            </ConfirmContextProvider>
+          </AuthContextProvider>
+        </ChakraProvider>
       </ApolloProvider>
     </>
   );
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,29 +1,7 @@
-import { ServerStyleSheets } from '@material-ui/core/styles';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import * as React from 'react';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
-    const sheets = new ServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
-
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App: any) => (props: any) =>
-          sheets.collect(<App {...props} />),
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      styles: [
-        ...React.Children.toArray(initialProps.styles),
-        sheets.getStyleElement(),
-      ],
-    };
-  }
-
   render() {
     return (
       <Html lang="en">

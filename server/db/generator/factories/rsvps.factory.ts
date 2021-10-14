bd528353643ab52246f44f13930a60762a8c2c69
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-10-14 16:02:00 +0200

    refactor: move interested flag to UserEventRole
    Instead of RSVP.interest, this makes it the responsibility of
    UserEventRole
    
    This makes RSVP responsible for information about the event (is a user
    going, canceled etc.), but leaves information about the app (roles and
    settings) in UserEventRole

--- server/db/generator/factories/rsvps.factory.ts
+++ server/db/generator/factories/rsvps.factory.ts
@@ -1,14 +1,21 @@
 import { date } from 'faker';
 import { random, randomItems } from '../lib/random';
-import { Event, User, Rsvp } from 'src/models';
+import { makeBooleanIterator } from '../lib/util';
+import { Event, User, Rsvp, UserEventRole } from 'src/models';
 
 const createRsvps = async (events: Event[], users: User[]): Promise<Rsvp[]> => {
   const rsvps: Rsvp[] = [];
+  const userEventRoles: UserEventRole[] = [];
 
+  const eventIterator = makeBooleanIterator();
   for (const event of events) {
     const eventUsers = randomItems(users, users.length / 2);
     const numberWaiting = 1 + random(eventUsers.length - 2);
     const numberCanceled = 1 + random(eventUsers.length - numberWaiting - 1);
+
+    // makes sure half of each event's users are organisers, but
+    // alternates which half.
+    const organizerIterator = makeBooleanIterator(eventIterator.next().value);
 
     for (let i = 0; i < eventUsers.length; i++) {
       const on_waitlist = i < numberWaiting;
@@ -22,7 +29,25 @@ const createRsvps = async (events: Event[], users: User[]): Promise<Rsvp[]> => {
         confirmed_at: new Date(),
       });
 
+      const attendee = new UserEventRole({
+        userId: eventUsers[i].id,
+        eventId: event.id,
+        roleName: 'attendee',
+        subscribed: true, // TODO: have some unsubscribed users
+      });
+
+      if (organizerIterator.next().value) {
+        const organizer = new UserEventRole({
+          userId: eventUsers[i].id,
+          eventId: event.id,
+          roleName: 'organizer',
+          subscribed: true, // TODO: even organizers may wish to opt out of emails
+        });
+        userEventRoles.push(organizer);
+      }
+
       rsvps.push(rsvp);
+      userEventRoles.push(attendee);
     }
   }
 
@@ -31,6 +56,15 @@ const createRsvps = async (events: Event[], users: User[]): Promise<Rsvp[]> => {
   } catch (e) {
     console.error(e);
     throw new Error('Error seeding rsvps');
+  }
+
+  try {
+    await Promise.all(
+      userEventRoles.map((userEventRole) => userEventRole.save()),
+    );
+  } catch (e) {
+    console.error(e);
+    throw new Error('Error seeding user-event-roles');
   }
 
   return rsvps;
--- server/src/models/UserEventRole.ts
+++ server/src/models/UserEventRole.ts
@@ -1,12 +1,12 @@
 import { Field, Int, ObjectType } from 'type-graphql';
-import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 import { BaseModel } from './BaseModel';
 import { Event } from './Event';
 import { User } from './User';
 
 // TODO: Make this enum, similarly to UserChapterRole
 
-type EventRoles = 'organizer';
+type EventRoles = 'organizer' | 'attendee';
 
 @ObjectType()
 @Entity({ name: 'user_event_roles' })
@@ -33,16 +33,22 @@ export class UserEventRole extends BaseModel {
   @PrimaryColumn({ type: 'text' })
   role_name!: EventRoles;
 
+  @Field(() => Boolean)
+  @Column({ nullable: false })
+  subscribed!: boolean;
+
   constructor(params: {
     userId: number;
     eventId: number;
     roleName: EventRoles;
+    subscribed: boolean;
   }) {
     super();
     if (params) {
       this.user_id = params.userId;
       this.event_id = params.eventId;
       this.role_name = params.roleName;
+      this.subscribed = params.subscribed;
     }
   }
 }

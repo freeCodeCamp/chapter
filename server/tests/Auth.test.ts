4342c25ba9184814c2230cec571e84b95ac1f300
Paul Gamble <pgamble@gmail.com>
2021-10-27 16:10:13 -0400

    first failed test

--- server/tests/Auth.test.ts
+++ server/tests/Auth.test.ts
@@ -1,5 +1,26 @@
 import assert from 'assert';
-import chai, { expect } from 'chai';
+//import chai, { expect } from 'chai';
+import chai from 'chai';
 import { stub, restore } from 'sinon';
 import sinonChai from 'sinon-chai';
-import { authTokenService } from 'src/services/AuthToken';
+//import { authTokenService } from 'src/services/AuthToken';
+
+chai.use(sinonChai);
+
+beforeEach(() => {
+  stub(console, 'warn');
+});
+
+afterEach(() => {
+  // Restore the default sandbox here
+  restore();
+});
+
+// Setup
+
+describe('First test', () => {
+  it('Should fail test', () => {
+    const tokenNum = '123456789';
+    assert.equal(tokenNum, 'abc');
+  });
+});

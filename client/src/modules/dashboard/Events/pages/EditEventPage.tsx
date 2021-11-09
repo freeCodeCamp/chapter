3c51696ee0d6f502fa3a12e344188e77e3349041
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-11-09 10:03:48 +0100

    fix: refetch edited event

--- client/src/modules/dashboard/Events/pages/EditEventPage.tsx
+++ client/src/modules/dashboard/Events/pages/EditEventPage.tsx
@@ -10,7 +10,7 @@ import { getId } from '../../../../helpers/getId';
 import { Layout } from '../../shared/components/Layout';
 import EventForm from '../components/EventForm';
 import { EventFormData } from '../components/EventFormUtils';
-import { EVENTS } from '../graphql/queries';
+import { EVENTS, EVENT } from '../graphql/queries';
 
 export const EditEventPage: NextPage = () => {
   const router = useRouter();
@@ -25,8 +25,10 @@ export const EditEventPage: NextPage = () => {
     variables: { id },
   });
 
+  // TODO: update the cache directly:
+  // https://www.apollographql.com/docs/react/data/mutations/#updating-the-cache-directly
   const [updateEvent] = useUpdateEventMutation({
-    refetchQueries: [{ query: EVENTS }],
+    refetchQueries: [{ query: EVENTS }, { query: EVENT, variables: { id } }],
   });
 
   const onSubmit = async (data: EventFormData) => {

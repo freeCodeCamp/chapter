174ffc365f4a86e97f153a3e6f169f3fe5a9dff7
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-11-15 11:28:16 +0100

    refactor: use Prisma for Chapter resolver

--- server/src/controllers/Chapter/resolver.ts
+++ server/src/controllers/Chapter/resolver.ts
@@ -1,14 +1,20 @@
+import { Prisma } from '@prisma/client';
 import { Resolver, Query, Arg, Int, Mutation } from 'type-graphql';
 import { CreateChapterInputs, UpdateChapterInputs } from './inputs';
-import { Chapter, User } from 'src/models';
+import { Chapter } from 'src/models';
+import { prisma } from 'src/prisma';
 
 @Resolver()
 export class ChapterResolver {
   @Query(() => [Chapter])
+  // TODO: add TypeGraphQL return type
   chapters() {
-    return Chapter.find();
+    return prisma.chapters.findMany();
   }
 
+  // TODO: add TypeGraphQL return type
+  // TODO: use Prisma once the schema names are updated (right now prisma is
+  // returning objects with the wrong name/shape)
   @Query(() => Chapter, { nullable: true })
   chapter(@Arg('id', () => Int) id: number) {
     return Chapter.findOne(id, {
@@ -16,45 +22,38 @@ export class ChapterResolver {
     });
   }
 
+  // TODO: add TypeGraphQL return type
   @Mutation(() => Chapter)
   async createChapter(@Arg('data') data: CreateChapterInputs) {
     // TODO: Use logged in user
-    const user = await User.findOne();
+    const user = await prisma.users.findFirst();
+    // TODO: fix the TypeGraphQL type, CreateChapterInputs (it should include
+    // details or the db should not require it)
+    // TODO: creator_id should not be optional and we shouldn't need Unchecked
+    // here
+    const chapterData: Prisma.chaptersUncheckedCreateInput = {
+      ...data,
+      details: data.details ?? '',
+      creator_id: user?.id,
+    };
 
-    const chapter = new Chapter({ ...data, creator: user });
-
-    return chapter.save();
+    return prisma.chapters.create({ data: chapterData });
   }
 
+  // TODO: add TypeGraphQL return type
   @Mutation(() => Chapter)
   async updateChapter(
     @Arg('id', () => Int) id: number,
     @Arg('data') data: UpdateChapterInputs,
   ) {
-    const chapter = await Chapter.findOne(id);
-
-    if (!chapter) throw new Error('Cant find chapter');
-
-    chapter.name = data.name ?? chapter.name;
-    chapter.description = data.description ?? chapter.description;
-    chapter.category = data.category ?? chapter.category;
-    chapter.details = data.details ?? chapter.details;
-    chapter.city = data.city ?? chapter.city;
-    chapter.region = data.region ?? chapter.region;
-    chapter.country = data.country ?? chapter.country;
-    chapter.imageUrl = data.imageUrl ?? chapter.imageUrl;
-
-    return chapter.save();
+    const chapterData: Prisma.chaptersUpdateInput = data;
+    return prisma.chapters.update({ where: { id }, data: chapterData });
   }
 
+  // TODO: add TypeGraphQL return type
   @Mutation(() => Boolean)
   async deleteChapter(@Arg('id', () => Int) id: number) {
-    const chapter = await Chapter.findOne(id);
-
-    if (!chapter) throw new Error('Cant find chapter');
-
-    await chapter.remove();
-
+    await prisma.chapters.delete({ where: { id } });
     return true;
   }
 }

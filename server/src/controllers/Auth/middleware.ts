f645d05f56414f05c9a7816ffa53205bcc77da60
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-11-15 19:54:03 +0100

    fix: update schema naming to match createEvent

--- server/src/controllers/Auth/middleware.ts
+++ server/src/controllers/Auth/middleware.ts
@@ -29,7 +29,7 @@ export const userMiddleware = (
   prisma.users
     .findUnique({
       where: { id: value.id },
-      include: { user_chapter_roles: true },
+      include: { chapter_roles: true },
     })
     .then((user) => {
       if (!user) {

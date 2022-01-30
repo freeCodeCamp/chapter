3e287878c7be97d131a351acda857ee3a8b6e263
gikf <60067306+gikf@users.noreply.github.com>
2022-01-30 10:21:55 +0100

    feat: throw by default when record not found

--- server/src/prisma.ts
+++ server/src/prisma.ts
@@ -3,4 +3,4 @@ import { PrismaClient } from '@prisma/client';
 // importing config so the .env gets parsed
 import 'src/config';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({ rejectOnNotFound: true });

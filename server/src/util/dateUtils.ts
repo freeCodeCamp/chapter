28b06928366bee5005c8c52e08a2ddb5fd81c633
gikf <60067306+gikf@users.noreply.github.com>
2022-02-15 12:33:05 +0100

    fix: use more general name

--- server/src/util/dateUtils.ts
+++ server/src/util/dateUtils.ts
@@ -2,7 +2,7 @@ export const getDateTimeNMinutesBefore = (
   from: Date,
   nMinutesBefore: number,
 ): Date => {
-  const remindDateTime = new Date(from);
-  remindDateTime.setMinutes(remindDateTime.getMinutes() - nMinutesBefore);
-  return remindDateTime;
+  const dateTime = new Date(from);
+  dateTime.setMinutes(dateTime.getMinutes() - nMinutesBefore);
+  return dateTime;
 };

d9952051104b79e3a9057f4e6f0607ac1984738d
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-03-15 15:46:29 +0100

    refactor: group extra types with relation fields together

--- server/src/graphql-types/EventWithChapter.ts
+++
@@ -1,8 +0,0 @@
-import { Field, ObjectType } from 'type-graphql';
-import { Chapter, Event } from '.';
-
-@ObjectType()
-export class EventWithChapter extends Event {
-  @Field(() => Chapter)
-  chapter: Chapter;
-}

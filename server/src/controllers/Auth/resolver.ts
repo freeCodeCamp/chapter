3735cc3f6689b9c2eaa50fa0aff7af70d937ea1a
Krzysztof G <60067306+gikf@users.noreply.github.com>
2022-03-16 18:56:43 +0100

    fix: querying for unique role names is allowed by db constraint
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- server/src/controllers/Auth/resolver.ts
+++ server/src/controllers/Auth/resolver.ts
@@ -39,7 +39,7 @@ export class AuthResolver {
       throw new Error('EMAIL_IN_USE');
     }
 
-    const instanceMember = await prisma.instance_roles.findFirst({
+    const instanceMember = await prisma.instance_roles.findUnique({
       where: { name: 'member' },
     });
 

94063de537e91812a314b79d5d0df0904b5df90b
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-03-30 11:14:06 +0200

    fix: handle string enums

--- server/prisma/generator/lib/random.ts
+++ server/prisma/generator/lib/random.ts
@@ -27,10 +27,21 @@ export const randomItems = <T>(arr: T[], n: number, uniq = true): T[] => {
   return out;
 };
 
-export const randomEnum = <T>(anEnum: Record<number, T>): T => {
-  const enumNumericKeys = Object.keys(anEnum)
+export const randomEnum = <T>(anEnum: Record<number | string, T>): T => {
+  const numericKeys = Object.keys(anEnum)
     .map((n) => Number.parseInt(n))
     .filter((n) => !Number.isNaN(n));
-  const randomIndex = Math.floor(Math.random() * enumNumericKeys.length);
-  return anEnum[randomIndex];
+  const stringKeys = Object.keys(anEnum).filter((s) => Number.isNaN(Number(s)));
+
+  if (numericKeys.length && stringKeys.length !== numericKeys.length)
+    throw new Error(
+      `This enum appears to be heterogeneous, if you're sure you need one, you should probably
+create a randomHeterogeneousEnum function`,
+    );
+  const randomIndex = Math.floor(Math.random() * stringKeys.length);
+  // String enums cannot have numeric keys, so if numeric keys exist, we can
+  // assume it is a numeric enum.
+  const keys = numericKeys.length ? numericKeys : stringKeys;
+
+  return anEnum[keys[randomIndex]];
 };

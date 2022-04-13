d32e98da681d94d23cf3841f6d88b8352fa2553e
gikf <60067306+gikf@users.noreply.github.com>
2022-04-13 22:07:36 +0200

    feat: service generating unsubscribe token

---
+++ server/src/services/UnsubscribeToken.ts
@@ -0,0 +1,33 @@
+import { sign } from 'jsonwebtoken';
+
+import { getConfig } from '../config';
+
+export enum unsubscribeType {
+  Chapter,
+  Event,
+  Instance,
+}
+
+export type UnsubscribeToken = {
+  type: unsubscribeType;
+  id: number;
+  user_id: number;
+  iat: number;
+  exp: number;
+};
+
+export const generateToken = (
+  type: unsubscribeType,
+  id: number,
+  user_id: number,
+) => {
+  return sign(
+    {
+      type: type,
+      id: id,
+      user_id: user_id,
+    },
+    getConfig('JWT_SECRET'),
+    { expiresIn: '14d' },
+  );
+};

8b398e99988c67f532e78568e73baf145012d9e9
gikf <60067306+gikf@users.noreply.github.com>
2022-04-19 21:46:21 +0200

    chore: add definitions for cypress custom commands

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,43 @@
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Intercept GQL request
+     * @param operationName Name of GQL operation to intercept
+     */
+    interceptGQL(operationName: string): void;
+
+    /**
+     * Get users of the chapter directly from server
+     * @param chapterId Id of the chapter
+     */
+    getChapterMembers(chapterId: number): Chainable<any>;
+
+    /**
+     * Get Rsvps for the event directly from server
+     * @param eventId Id of the event
+     */
+    getRSVPs(eventId: number): Chainable<any>;
+
+    /**
+     * Authenticate with JWT token
+     * @param token JWT token for authorization. If not provided, Cypress.env.JWT token is used.
+     */
+    login(token?: string): void;
+
+    /**
+     * Register user using GQL query
+     */
+    register(firstName: string, lastName: string, email: string): void;
+
+    /**
+     * Register user using page UI
+     */
+    registerViaUI(firstName: string, lastName: string, email: string): void;
+
+    /**
+     * Wait until emails are received by mailhog
+     * @param alias Name of the alias to reference emails by
+     */
+    waitUntilMail(alias: string): void;
+  }
+}

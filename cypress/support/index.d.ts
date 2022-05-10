2f26f8bcaa95911f256b2efa3df4fd129e6b9ed5
gikf <60067306+gikf@users.noreply.github.com>
2022-05-10 22:04:24 +0200

    command declarations

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -39,5 +39,17 @@ declare namespace Cypress {
      * @param alias Name of the alias to reference emails by
      */
     waitUntilMail(alias: string): void;
+
+    /**
+     * Create event using GQL mutation
+     * @param data Data of the event. Equivalent of CreateEventInputs for the Events resolver.
+     */
+    createEvent(data): Chainable<any>;
+
+    /**
+     * Delete event using GQL mutation
+     * @param eventId Id of the event for deletion
+     */
+    deleteEvent(eventId: number): Chainable<any>;
   }
 }

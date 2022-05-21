d77c9048cbc5272c7ecf5494f91c77f654994425
gikf <60067306+gikf@users.noreply.github.com>
2022-05-21 19:36:28 +0200

    fix: more flexible tests

--- cypress/integration/dashboard/events/[eventId].js
+++ cypress/integration/dashboard/events/[eventId].js
@@ -7,36 +7,34 @@ describe('event dashboard', () => {
   describe('users lists', () => {
     it('confirming user on waitlist should move user to RSVPs', () => {
       cy.visit('/dashboard/events/1');
-      cy.contains(/Waitlist/);
-      cy.findAllByRole('row')
-        .filter(':contains("Confirm")')
+      cy.get('[data-cy=waitlist]').as('waitlist');
+      cy.get('@waitlist')
+        .find('[data-cy=username]')
         .first()
-        .as('waitlistRow');
-      cy.get('@waitlistRow').find('td').first().invoke('text').as('userName');
-      cy.get('@waitlistRow')
-        .contains(/Confirm/)
-        .click();
+        .invoke('text')
+        .as('userName');
+
+      cy.get('@waitlist').find('[data-cy=confirmRSVP]').first().click();
       cy.findByRole('alertdialog')
         .findByRole('button', { name: 'Confirm' })
         .click();
 
       cy.get('@userName').then((userName) => {
-        cy.get('@waitlistRow').not(`:contains(${userName})`);
-        cy.findAllByRole('row')
-          .filter(`:contains(${userName})`)
-          .first()
-          .as('userRow');
-        cy.get('@userRow').not(':contains("Confirm")');
-        cy.get('@userRow').contains(/Kick/);
+        cy.get('@waitlist').not(`:contains(${userName})`);
+        cy.get('[data-cy=rsvps]').contains(userName);
       });
     });
 
     it('kicking user should remove user from event', () => {
       cy.visit('/dashboard/events/1');
-      cy.contains(/RSVPs/);
-      cy.findAllByRole('row').filter(':contains("Kick")').first().as('rsvpRow');
-      cy.get('@rsvpRow').find('td').first().invoke('text').as('userName');
-      cy.get('@rsvpRow').contains(/Kick/).click();
+      cy.get('[data-cy=rsvps]').as('rsvps');
+      cy.get('@rsvps')
+        .find('[data-cy=username]')
+        .first()
+        .invoke('text')
+        .as('userName');
+
+      cy.get('@rsvps').find('[data-cy=kick]').first().click();
       cy.findByRole('button', { name: 'Delete' }).click();
 
       cy.get('@userName').then((userName) => {
@@ -46,43 +44,44 @@ describe('event dashboard', () => {
 
     it('canceling confirming user on waitlist should not move user to RSVPs', () => {
       cy.visit('/dashboard/events/1');
-      cy.contains(/Waitlist/);
-      cy.findAllByRole('row')
-        .filter(':contains("Confirm")')
+      cy.get('[data-cy=waitlist]').as('waitlist');
+      cy.get('@waitlist')
+        .find('[data-cy=username]')
         .first()
-        .as('waitlistRow');
-      cy.get('@waitlistRow').find('td').first().invoke('text').as('userName');
-      cy.get('@waitlistRow')
-        .contains(/Confirm/)
-        .click();
+        .invoke('text')
+        .as('userName');
+
+      cy.get('@waitlist').find('[data-cy=confirmRSVP]').first().click();
+
       cy.intercept('/graphql', cy.spy().as('request'));
       cy.findByRole('alertdialog')
         .findByRole('button', { name: 'Cancel' })
         .click();
 
       cy.get('@request').should('not.have.been.called');
-
       cy.get('@userName').then((userName) => {
-        cy.get('@waitlistRow').contains(userName);
+        cy.get('@waitlist').contains(userName);
       });
     });
 
     it('canceling kicking user should not remove user from event', () => {
       cy.visit('/dashboard/events/1');
-      cy.contains(/Waitlist/);
-      cy.findAllByRole('row').filter(':contains("Kick")').first().as('rsvpRow');
-      cy.get('@rsvpRow').find('td').first().invoke('text').as('userName');
-      cy.get('@rsvpRow').contains(/Kick/).click();
+      cy.get('[data-cy=rsvps]').as('rsvps');
+      cy.get('@rsvps')
+        .find('[data-cy=username]')
+        .first()
+        .invoke('text')
+        .as('userName');
+
+      cy.get('@rsvps').find('[data-cy=kick]').first().click();
       cy.intercept('/graphql', cy.spy().as('request'));
       cy.findByRole('alertdialog')
         .findByRole('button', { name: 'Cancel' })
         .click();
 
       cy.get('@request').should('not.have.been.called');
-
       cy.get('@userName').then((userName) => {
-        cy.contains(userName);
-        cy.get('@rsvpRow').contains(userName);
+        cy.get('@rsvps').contains(userName);
       });
     });
   });

a00032d2898f6819b1235b58a6e0c02be34a500e
gikf <60067306+gikf@users.noreply.github.com>
2022-05-22 21:05:45 +0200

    feat: changing role resolver

--- server/src/controllers/ChapterUser/resolver.ts
+++ server/src/controllers/ChapterUser/resolver.ts
@@ -3,6 +3,7 @@ import { Arg, Ctx, Int, Mutation, Resolver } from 'type-graphql';
 
 import { GQLCtx } from '../../common-types/gql';
 import { prisma } from '../../prisma';
+import { ChapterUser } from '../../graphql-types';
 
 const UNIQUE_CONSTRAINT_FAILED_CODE = 'P2002';
 
@@ -44,5 +45,34 @@ export class ChapterUserResolver {
     }
 
     return true;
+  }
+
+  @Mutation(() => ChapterUser)
+  async changeChapterUserRole(
+    @Arg('chapterId', () => Int) chapterId: number,
+    @Arg('roleId', () => Int) roleId: number,
+    @Arg('userId', () => Int) userId: number,
+    @Ctx() ctx: GQLCtx,
+  ): Promise<ChapterUser> {
+    if (!ctx.user) {
+      throw Error('User must be logged in to change chapter role');
+    }
+    return await prisma.chapter_users.update({
+      data: { chapter_role: { connect: { id: roleId } } },
+      where: {
+        user_id_chapter_id: {
+          chapter_id: chapterId,
+          user_id: userId,
+        },
+      },
+      include: {
+        chapter_role: {
+          include: {
+            chapter_role_permissions: { include: { chapter_permission: true } },
+          },
+        },
+        user: true,
+      },
+    });
   }
 }

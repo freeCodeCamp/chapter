f0366781567276bc81cc69f7e7543fc29475ca00
gikf <60067306+gikf@users.noreply.github.com>
2022-05-31 12:18:12 +0200

    test: check rsvp confirm email is send

--- cypress/integration/dashboard/events/[eventId].js
+++ cypress/integration/dashboard/events/[eventId].js
@@ -5,7 +5,7 @@ describe('event dashboard', () => {
   });
 
   describe('users lists', () => {
-    it('confirming user on waitlist should move user to RSVPs', () => {
+    it('confirming user on waitlist should move user to RSVPs and send email', () => {
       cy.visit('/dashboard/events/1');
       cy.get('[data-cy=waitlist]').as('waitlist');
       cy.get('@waitlist')
@@ -19,9 +19,27 @@ describe('event dashboard', () => {
         .findByRole('button', { name: 'Confirm' })
         .click();
 
+      cy.waitUntilMail('allMail');
+      cy.get('@allMail').mhFirst().as('email');
+
       cy.get('@userName').then((userName) => {
         cy.get('@waitlist').not(`:contains(${userName})`);
         cy.get('[data-cy=rsvps]').contains(userName);
+      });
+
+      cy.get('@email')
+        .mhGetSubject()
+        .should('include', 'Your RSVP is confirmed');
+      cy.get('@email')
+        .mhGetBody()
+        .should('include', 'reservation is confirmed');
+      cy.getEventUsers(1).then((eventUsers) => {
+        cy.get('@userName').then((userName) => {
+          const userEmail = eventUsers
+            .filter(({ user: { name } }) => name === userName)
+            .map(({ user: { email } }) => email);
+          cy.get('@email').mhGetRecipients().should('have.members', userEmail);
+        });
       });
     });
 

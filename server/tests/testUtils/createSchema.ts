59482c51dd304993117bba3d6408b90afd0362db
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-06-14 14:37:33 +0200

    test(fix): add authChecker to test schema

--- server/tests/testUtils/createSchema.ts
+++ server/tests/testUtils/createSchema.ts
@@ -1,8 +1,10 @@
 import { buildSchema } from 'type-graphql';
+import { authorizationChecker } from '../../src/authorization';
 
 import { resolvers } from '../../src/controllers';
 
 export const createSchema = () =>
   buildSchema({
     resolvers,
+    authChecker: authorizationChecker,
   });

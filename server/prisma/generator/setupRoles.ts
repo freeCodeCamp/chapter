ee35adcd795426a25c6146ba92927dae48ac0dcd
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-06-24 09:41:47 +0200

    fix: remove misleading comment
    Co-authored-by: Krzysztof G <60067306+gikf@users.noreply.github.com>

--- server/prisma/generator/setupRoles.ts
+++ server/prisma/generator/setupRoles.ts
@@ -33,11 +33,7 @@ const setupRoles = async (
       joined_date: new Date(),
       chapter_id: chapterId,
       user_id: adminId,
-      chapter_role_id: chapterRoles.administrator.id, // This user is an instance owner
-      // so this chapter role should not provide additional permissions beyond
-      // those provided by the instance owner role. It is possible for them to
-      // be a member of a chapater, though, so this grants them the member role
-      // for all chapters.
+      chapter_role_id: chapterRoles.administrator.id,
       subscribed: true,
     };
 

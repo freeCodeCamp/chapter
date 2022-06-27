ee02cc63b43012803e87d1794afa47d290a68c1c
gikf <60067306+gikf@users.noreply.github.com>
2022-06-27 22:04:35 +0200

    feat: unsubscribing resolver

--- server/src/controllers/Unsubscribe/resolver.ts
+++ server/src/controllers/Unsubscribe/resolver.ts
@@ -9,23 +9,44 @@ import {
   unsubscribeType,
 } from '../../services/UnsubscribeToken';
 
-const unsubscribeChapter = async (userId: number, chapterId: number) => {
+const unsubscribeFromChapter = async (userId: number, chapterId: number) => {
+  const chapter = await prisma.chapters.findUnique({
+    where: { id: chapterId },
+    include: { events: true },
+  });
   await prisma.chapter_users.update({
     data: { subscribed: false },
     where: { user_id_chapter_id: { chapter_id: chapterId, user_id: userId } },
   });
+  const onlyUserEventsFromChapter = {
+    AND: [
+      { user_id: userId },
+      { event_id: { in: chapter.events.map(({ id }) => id) } },
+    ],
+  };
+
+  await prisma.event_users.updateMany({
+    data: { subscribed: false },
+    where: onlyUserEventsFromChapter,
+  });
+  await prisma.event_reminders.deleteMany({
+    where: onlyUserEventsFromChapter,
+  });
 };
 
-const unsubscribeEvent = async (userId: number, eventId: number) => {
+const unsubscribeFromEvent = async (userId: number, eventId: number) => {
   await prisma.event_users.update({
     data: { subscribed: false },
     where: { user_id_event_id: { event_id: eventId, user_id: userId } },
   });
+  await prisma.event_reminders.delete({
+    where: { user_id_event_id: { user_id: userId, event_id: eventId } },
+  });
 };
 
 const typeToUnsubscribe = {
-  [unsubscribeType.Chapter]: unsubscribeChapter,
-  [unsubscribeType.Event]: unsubscribeEvent,
+  [unsubscribeType.Chapter]: unsubscribeFromChapter,
+  [unsubscribeType.Event]: unsubscribeFromEvent,
 };
 
 @Resolver()
@@ -39,7 +60,7 @@ export class UnsubscribeResolver {
       throw Error('Invalid token');
     }
 
-    await typeToUnsubscribe[data.type](data.user_id, data.id);
+    await typeToUnsubscribe[data.type](data.userId, data.id);
 
     return true;
   }

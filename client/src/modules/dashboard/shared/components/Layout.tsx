d39562d5e4dac4fb1bfcf8f08a7f4f23fa9cc113
gikf <60067306+gikf@users.noreply.github.com>
2022-07-06 21:54:18 +0200

    feat: display sponsors dashboard button when having permission

--- client/src/modules/dashboard/shared/components/Layout.tsx
+++ client/src/modules/dashboard/shared/components/Layout.tsx
@@ -2,12 +2,17 @@ import { HStack } from '@chakra-ui/layout';
 import { LinkButton } from 'chakra-next-link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { useCheckPermission } from 'hooks/useCheckPermission';
 
 const links = [
   { text: 'Chapters', link: '/dashboard/chapters' },
   { text: 'Events', link: '/dashboard/events' },
   { text: 'Venues', link: '/dashboard/venues' },
-  { text: 'Sponsors', link: '/dashboard/sponsors' },
+  {
+    text: 'Sponsors',
+    link: '/dashboard/sponsors',
+    permission: 'sponsors-manage',
+  },
   { text: 'Users', link: '/dashboard/users' },
 ];
 
@@ -17,17 +22,23 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <HStack as="nav" my="2">
-        {links.map((item) => (
-          <LinkButton
-            key={item.link}
-            href={item.link}
-            colorScheme={
-              router.pathname.startsWith(item.link) ? 'blue' : undefined
-            }
-          >
-            {item.text}
-          </LinkButton>
-        ))}
+        {links
+          .filter(
+            ({ permission }) =>
+              typeof permission === 'undefined' ||
+              useCheckPermission(permission),
+          )
+          .map((item) => (
+            <LinkButton
+              key={item.link}
+              href={item.link}
+              colorScheme={
+                router.pathname.startsWith(item.link) ? 'blue' : undefined
+              }
+            >
+              {item.text}
+            </LinkButton>
+          ))}
       </HStack>
       {children}
     </>

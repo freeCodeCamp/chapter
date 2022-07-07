b17332a36c3f16163a6c0305ada7012f579641e8
moshe vilner <shootermv@gmail.com>
2022-07-07 13:42:18 +0300

    fix: requested changes according the comments

--- cypress/e2e/dashboard/events/index.cy.js
+++ cypress/e2e/dashboard/events/index.cy.js
@@ -1,5 +1,3 @@
-import { aliasQuery } from '../../utils/graphql-test-utils';
-
 const testEvent = {
   title: 'Test Event',
   description: 'Test Description',
@@ -18,15 +16,11 @@ describe('events dashboard', () => {
     cy.exec('npm run db:seed');
     cy.login();
     cy.mhDeleteAll();
-    cy.intercept('POST', 'http://localhost:5000/graphql', (req) => {
-      // Queries
-      aliasQuery(req, 'events');
-    });
+    cy.interceptGQL('events');
   });
 
   it('should be the active dashboard link', () => {
     cy.visit('/dashboard/');
-    //cy.get('a[aria-current="page"]').should('have.text', 'Dashboard');
     cy.get('a[href="/dashboard/events"]').click();
     cy.get('a[aria-current="page"]').should('have.text', 'Events');
   });
@@ -276,7 +270,7 @@ describe('events dashboard', () => {
     cy.findByRole('link', { name: 'Dashboard' }).click();
 
     cy.findByRole('link', { name: 'Events' }).click();
-    cy.wait('@gqleventsQuery');
+    cy.wait('@GQLevents');
     cy.url().should('include', '/events');
     cy.get('#page-heading').contains('Events');
     cy.contains('Loading...').should('not.exist');
@@ -313,7 +307,7 @@ describe('events dashboard', () => {
 
     cy.findByRole('link', { name: 'Dashboard' }).click();
     cy.findByRole('link', { name: 'Events' }).click();
-    cy.wait('@gqleventsQuery');
+    cy.wait('@GQLevents');
     cy.get('#page-heading').contains('Events');
     cy.contains('Loading...').should('not.exist');
     cy.get('@eventTitle').then((eventTitle) => {

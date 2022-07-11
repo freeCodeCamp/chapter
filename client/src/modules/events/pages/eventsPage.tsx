4edb97697e1b3781fa57fe9f34aa9063b4c626be
moshe vilner <shootermv@gmail.com>
2022-07-11 10:51:41 +0300

    feat: add pagination to event-feed page

--- client/src/modules/events/pages/eventsPage.tsx
+++ client/src/modules/events/pages/eventsPage.tsx
@@ -1,11 +1,65 @@
-import { Heading, VStack, Stack } from '@chakra-ui/layout';
+import { Heading, VStack, Stack, Center } from '@chakra-ui/layout';
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { Button, Box, Flex } from '@chakra-ui/react';
 import { EventCard } from 'components/EventCard';
 import { useMinEventsQuery } from 'generated/graphql';
 
+export default function Pagination({
+  currentPage = 1,
+  setCurrentPage,
+  pageSize,
+  records,
+}: {
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
+  pageSize: number;
+  records: number;
+}) {
+  const totalPages = Math.ceil(records / pageSize);
+
+  return (
+    <Flex>
+      <Center>
+        <Button
+          colorScheme="blue"
+          data-testid="pagination-back"
+          disabled={!(currentPage > 1)}
+          onClick={() => currentPage > 1 && setCurrentPage(currentPage - 1)}
+        >
+          &lt;
+        </Button>
+        <Box p="5">Page</Box>
+        <Box p="5" data-testid="current-page">
+          {currentPage}
+        </Box>
+        <Box p="5">of</Box>
+        <Box p="5" data-testid="total-pages">
+          {totalPages}
+        </Box>
+        <Button
+          colorScheme="blue"
+          data-testid="pagination-forward"
+          disabled={!(currentPage < totalPages)}
+          onClick={() =>
+            currentPage < totalPages && setCurrentPage(currentPage + 1)
+          }
+        >
+          &gt;
+        </Button>
+      </Center>
+    </Flex>
+  );
+}
+
 export const EventsPage: NextPage = () => {
   const { loading, error, data } = useMinEventsQuery();
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 5;
+  const paginated = useMemo(() => {
+    const offset = (currentPage - 1) * pageSize;
+    return data?.events.slice(offset, offset + pageSize); // 0 , 4 // 5, 10
+  }, [currentPage, pageSize, data?.events]);
 
   if (loading) {
     return <h1>Loading...</h1>;
@@ -24,9 +78,15 @@ export const EventsPage: NextPage = () => {
     <VStack>
       <Stack w={['90%', '90%', '60%']} maxW="600px" spacing={6} mt={10} mb={5}>
         <Heading>Events: </Heading>
-        {data.events.map((event) => (
+        {paginated?.map((event) => (
           <EventCard key={event.id} event={event} />
         ))}
+        <Pagination
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          pageSize={pageSize}
+          records={data.events.length}
+        />
       </Stack>
     </VStack>
   );
--- cypress/e2e/events/index.cy.js
+++ cypress/e2e/events/index.cy.js
@@ -1,14 +1,17 @@
 describe('events page', () => {
   before(() => {
     cy.exec('npm run db:seed');
+    cy.interceptGQL('minEvents');
   });
 
   it('should contain a list of event cards', () => {
     cy.visit('/events');
+    cy.wait('@GQLminEvents');
     cy.get('[data-cy="event-card"]')
       .should('be.visible')
-      .should('have.length', 15)
+      .should('have.length', 5)
       .as('eventCards');
+
     cy.get('@eventCards').find('[data-cy="event-canceled"]').should('exist');
     cy.get('@eventCards').find('[data-cy="event-invite-only"]').should('exist');
   });

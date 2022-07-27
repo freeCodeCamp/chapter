a8057fbfcb8bd55e85e2d98f63a0a9cb9400bdfe
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-07-27 10:08:51 +0200

    fix: stop creating session while seeding

--- server/prisma/generator/seed.ts
+++ server/prisma/generator/seed.ts
@@ -7,7 +7,6 @@ import createSponsors from './factories/sponsors.factory';
 import createUsers from './factories/user.factory';
 import createVenues from './factories/venues.factory';
 import setupRoles from './setupRoles';
-import createSession from './factories/session.factory';
 
 (async () => {
   const tablenames = await prisma.$queryRaw<
@@ -48,5 +47,4 @@ import createSession from './factories/session.factory';
     chapterIds,
     chapterRoles,
   );
-  await createSession(ownerId);
 })();

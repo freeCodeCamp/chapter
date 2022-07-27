aaf2cacab3118dd24c4bcc3e7ac2f897a5839d10
moshe vilner <shootermv@gmail.com>
2022-07-27 16:18:22 +0300

    feat: name users after Harry Potter characters

--- server/prisma/generator/factories/user.factory.ts
+++ server/prisma/generator/factories/user.factory.ts
@@ -3,7 +3,7 @@ import { Prisma } from '@prisma/client';
 
 import { prisma } from '../../../src/prisma';
 
-const { name, internet } = faker;
+const { internet } = faker;
 
 const createUsers = async (
   instanceRoles: Record<string, { name: string; id: number }>,
@@ -15,35 +15,48 @@ const createUsers = async (
 }> => {
   const ownerData: Prisma.usersCreateInput = {
     email: 'foo@bar.com',
-    first_name: name.firstName(),
-    last_name: name.lastName(),
+    first_name: 'Albus',
+    last_name: 'Dumbledore',
     instance_role: { connect: { id: instanceRoles.owner.id } },
   };
   const owner = await prisma.users.create({ data: ownerData });
 
   const adminData: Prisma.usersCreateInput = {
     email: 'admin@of.a.chapter',
-    first_name: name.firstName(),
-    last_name: name.lastName(),
+    first_name: 'Minerva',
+    last_name: 'McGonagall',
     instance_role: { connect: { id: instanceRoles.member.id } },
   };
   const admin = await prisma.users.create({ data: adminData });
 
   const bannedAdminData: Prisma.usersCreateInput = {
     email: 'banned@chapter.admin',
-    first_name: name.firstName(),
-    last_name: name.lastName(),
+    first_name: 'Lord',
+    last_name: 'Voldemort',
     instance_role: { connect: { id: instanceRoles.member.id } },
   };
 
   const bannedAdmin = await prisma.users.create({ data: bannedAdminData });
 
+  const userNames = [
+    { firstName: 'Dudley', lastName: 'Dursley' },
+    { firstName: 'Petunia', lastName: 'Dursley' },
+    { firstName: 'Vernon', lastName: 'Dursley' },
+    { firstName: 'Drako', lastName: 'Malfoy' },
+    { firstName: 'Hermione', lastName: 'Granger' },
+    { firstName: 'Ron', lastName: 'Weasley' },
+    { firstName: 'James', lastName: 'Potter' },
+    { firstName: 'Harry', lastName: 'Potter' },
+    { firstName: 'Neville', lastName: 'Longbottom' },
+    { firstName: 'Severus', lastName: 'Snape' },
+  ];
+
   const othersData: Prisma.usersCreateInput[] = Array.from(
-    new Array(10),
-    () => ({
+    new Array(10).keys(),
+    (idx: number) => ({
       email: internet.email(),
-      first_name: name.firstName(),
-      last_name: name.lastName(),
+      first_name: userNames[idx].firstName,
+      last_name: userNames[idx].lastName,
       instance_role: { connect: { id: instanceRoles.member.id } },
     }),
   );

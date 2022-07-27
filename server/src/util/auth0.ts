e62d4ebdb62d5fa3c507eeadad3b63a86950ebf9
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-07-27 10:39:27 +0200

    fix: stop caching dev-data

--- server/src/util/auth0.ts
+++ server/src/util/auth0.ts
@@ -1,13 +1,15 @@
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
 import fetch from 'node-fetch';
-import { isDev } from '../config';
 
-import { email } from '../../dev-data.json';
+import { isDev } from '../config';
 
 export function fetchUserInfo(token: string): Promise<{ email?: string }> {
   // TODO: the dev-only code is scattered across several files. Is it possible
   // to use, say, passport and confine the code to individual strategies?
   if (isDev()) {
-    return Promise.resolve({ email });
+    const devDataFile = resolve(__dirname, '../../dev-data.json');
+    return readFile(devDataFile, 'utf8').then(JSON.parse);
   } else {
     return fetch(`https://${process.env.AUTH0_DOMAIN}/userinfo`, {
       headers: {

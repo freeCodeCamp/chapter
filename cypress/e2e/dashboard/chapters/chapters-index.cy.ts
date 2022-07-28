4eb7c90d346ddb3d72197305c5f475895de6afaf
Moshe <shootermv@gmail.com>
2022-07-28 10:23:32 +0300

    Update cypress/e2e/dashboard/chapters/chapters-index.cy.ts
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- cypress/e2e/dashboard/chapters/chapters-index.cy.ts
+++ cypress/e2e/dashboard/chapters/chapters-index.cy.ts
@@ -63,7 +63,7 @@ describe('chapters dashboard', () => {
   });
 
   it('only allows owners to create chapters', () => {
-    cy.login(Cypress.env('CHAPTER_1_OWNER'));
+    cy.login(Cypress.env('JWT_CHAPTER_1_ADMIN'));
 
     cy.visit('/dashboard/chapters');
     cy.get('[data-cy="new-chapter"]').should('not.exist');

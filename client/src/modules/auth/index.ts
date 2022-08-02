7a3f39734dfac2ee560f09aa0c4144d41d459d18
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-08-02 11:58:43 +0200

    fix: export ConfirmPage

--- client/src/modules/auth/index.ts
+++ client/src/modules/auth/index.ts
@@ -1,3 +1,4 @@
 export { LoginPage } from './pages/Login';
 export { TokenPage } from './pages/Token';
 export { RegisterPage } from './pages/Register';
+export { ConfirmPage } from './pages/Confirm';

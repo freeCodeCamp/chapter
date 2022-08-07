d6c781e72d163b9b173b93a0a32fca209322e754
Sboonny <muhammed@freecodecamp.org>
2022-08-07 17:28:30 +0000

    made grid

--- client/src/components/ChapterCard.tsx
+++ client/src/components/ChapterCard.tsx
@@ -1,12 +1,4 @@
-import {
-  Stack,
-  Heading,
-  Box,
-  Center,
-  Image,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Stack, Heading, Box, Grid, Image, Text, Tag } from '@chakra-ui/react';
 import { Link } from 'chakra-next-link';
 import React from 'react';
 
@@ -21,22 +13,17 @@ type ChapterCardProps = {
 
 export const ChapterCard: React.FC<ChapterCardProps> = ({ chapter }) => {
   return (
-    <Center m={2} data-cy="chapter-card">
+    <Grid gridGap={2} data-cy="chapter-card">
       <Link href={`/chapters/${chapter.id}`} _hover={{}}>
-        <Box
-          w={'full'}
-          bg={useColorModeValue('white', 'gray.800')}
-          rounded={'md'}
-          overflow={'hidden'}
+        <Grid
+          gridGap={2}
+          w={'100vw'}
+          maxW={'40em'}
+          templateColumns="repeat(2, 1fr)"
+          boxShadow="outline"
         >
-          <Image
-            h={'168px'}
-            w={'full'}
-            src={chapter.imageUrl}
-            objectFit={'cover'}
-          />
           <Box>
-            <Stack spacing={0} align={'center'} mb={5} mt={-20}>
+            <Stack spacing={0} boxShadow="outline">
               <Heading
                 data-cy="chapter-heading"
                 fontSize={'xl'}
@@ -50,10 +37,18 @@ export const ChapterCard: React.FC<ChapterCardProps> = ({ chapter }) => {
               <Text color={'white'} textShadow={'1px 1px #000'}>
                 {chapter.category}
               </Text>
+              <Tag>Random Tag</Tag>
             </Stack>
           </Box>
-        </Box>
+          <Image
+            boxSize={'100px'}
+            src={chapter.imageUrl}
+            objectFit={'cover'}
+            display="block"
+          />
+          <Text boxShadow="dark-lg">{chapter.description}</Text>
+        </Grid>
       </Link>
-    </Center>
+    </Grid>
   );
 };

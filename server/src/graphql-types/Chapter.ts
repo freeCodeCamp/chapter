d6c938caae7e2c06bb22543b6bafe9916d688502
Sboonny <muhammed@freecodecamp.org>
2022-08-11 07:46:45 +0000

    changed the last field

--- server/src/graphql-types/Chapter.ts
+++ server/src/graphql-types/Chapter.ts
@@ -25,8 +25,8 @@ export class Chapter extends BaseObject {
   @Field(() => String)
   country: string;
 
-  @Field(() => String)
-  tag: string;
+  @Field(() => [String])
+  tag: string[];
 
   @Field(() => String)
   imageUrl!: string;

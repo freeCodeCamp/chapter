c846bacd8a2e3bbaa72dbbc3b4ca05c070c10a5e
Sboonny <muhammed@freecodecamp.org>
2022-08-13 13:48:17 +0000

    added themes configs

---
+++ client/src/styles/themes.ts
@@ -0,0 +1,19 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const chapterStyleVaribles = {
+  // colors: {
+  //     gray: {
+  //     00: #ffffff,
+  //     05: #f5f6f7,
+  //     10: #dfdfe2,
+  //     15: #d0d0d5,
+  //     45: #858591,
+  //     75: #3b3b4f,
+  //     80: #2a2a40,
+  //     85: #1b1b32,
+  //     90: #0a0a23,
+  //   }
+  // }
+};
+
+export const chapterTheme = extendTheme(chapterStyleVaribles);

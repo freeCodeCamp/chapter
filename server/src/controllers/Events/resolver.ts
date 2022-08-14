7bf678d1b771f2351cf0ddb370a27641ee0743d8
moshe vilner <shootermv@gmail.com>
2022-08-14 18:23:56 +0300

    feat: add send-even-invite permission

--- server/src/controllers/Events/resolver.ts
+++ server/src/controllers/Events/resolver.ts
@@ -740,7 +740,7 @@ ${venueDetails}`;
 
   // TODO: This will need a real GraphQL return type (AFAIK you have to return
   // an object type)
-  @Authorized(Permission.EventEdit)
+  @Authorized(Permission.EventSendInvite)
   @Mutation(() => Boolean)
   async sendEventInvite(
     @Arg('id', () => Int) id: number,

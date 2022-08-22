9ce46582edceda524038340eea075b823c5da3df
Sboonny <muhammed@freecodecamp.org>
2022-08-22 05:54:44 +0000

    confIrm page styling

--- client/src/modules/auth/pages/Confirm.tsx
+++ client/src/modules/auth/pages/Confirm.tsx
@@ -1,16 +1,30 @@
 import { NextPage } from 'next';
 import React from 'react';
-import { Box, VStack, Heading } from '@chakra-ui/react';
+import { Box, VStack, Heading, Input, Text } from '@chakra-ui/react';
 
 export const ConfirmPage: NextPage = () => {
   return (
     <Box w="50%" maxW="800px" marginX="auto" mt="10">
       <form>
         <VStack>
-          <Heading>Confirm</Heading>
-          <p>Please Type your password to confirm</p>
-          <input type="password" />
-          <input type="submit" />
+          <Heading as="h1">Confirm</Heading>
+          <Text fontSize={'large'}>Please Type your password to confirm</Text>
+          <Input
+            background={'gray.15'}
+            color={'gray.90'}
+            type="password"
+            required
+          />
+          <Input
+            type="submit"
+            paddingInline={2}
+            paddingBlock={1}
+            maxW={'15em'}
+            fontWeight={500}
+            textTransform={'uppercase'}
+            background={'gray.85'}
+            color={'gray.10'}
+          />
         </VStack>
       </form>
     </Box>

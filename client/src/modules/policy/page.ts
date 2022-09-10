db6d4df3cb5a5296441e9663f7ecf7abcbed7118
Sboonny <muhammed@freecodecamp.org>
2022-09-10 14:02:44 +0000

    initial commit

---
+++ client/src/modules/policy/page.ts
@@ -0,0 +1,3 @@
+import { PolicyPage } from './pages/Policy';
+
+export default PolicyPage;
---
+++ client/src/modules/policy/pages/Policy.tsx
@@ -0,0 +1,135 @@
+import React from 'react';
+
+export const PolicyPage = () => {
+  return (
+    <div>
+      <h1>
+        We take your privacy seriously. And we give you full control over your
+        data.
+      </h1>
+
+      <h2>Does Chapter collect anonymous data?</h2>
+      <p>
+        When you use the Chapter website, we may collect some anonymous data so
+        we can understand how people are using Chapter, and basic facts like
+        which browser they&aposre using.
+      </p>
+
+      <h2>In what situations does Chapter collect personal data?</h2>
+      <p>
+        If you create a Chapter account, we will collect some personal data so
+        we can follow your progress toward earning developer certifications, and
+        so you can customize your developer portfolio.
+      </p>
+
+      <h2>Can I use Chapter anonymously?</h2>
+      <p>
+        Yes. You can access all of Chapter&aposs events and interactive Chapter
+        without creating an account. And if you don&apost create an account, we
+        won&apost collect any personal data about you.
+      </p>
+
+      <h2>If I create an account, what data will you collect?</h2>
+      <p>
+        We&aposll ask you for your email address so you can use it to sign into
+        Chapter, and so we can provide...
+      </p>
+
+      <p>
+        When you create an account on Chapter, we publish a organizer portfolio
+        page for you on Chapter. If you want, you can add details about
+        yourself, like your name, geographic location, and a link to your
+        personal website.
+      </p>
+
+      <p>
+        By default, your organizer portfolio will show which Chapter event you
+        have organized.
+      </p>
+
+      <p>
+        You have full control over your data, and can set any of these details
+        to private, or delete them at any time.
+      </p>
+
+      {/* commeted these because we don&apost have way to do them, yet.
+<h2>You said I have full control over my data. What does that mean, exactly?</h2>
+<p>It means that at any time, you can download all of your data in a convenient format.</p> 
+
+<p>You can control which data shows up on your organizer portfolio.</p>
+
+<p>You can delete any of your personal data, or even delete your entire account.</p>
+
+<p>For your convenience, we give you the option to sign in using GitHub or Google, if you don&apost want to use your email address to sign in. If you choose to use one of these sign in options, some of your Chapter data will be shared with these companies. You can read the privacy policy for GitHub and for Google.</p>
+
+<h2>I have questions about my privacy on Chapter.</h2>
+<p>We&aposre happy to answer them. Email us at email.</p>
+
+<h2>How can I find out about changes?</h2>
+<p>This version of Chapter’s privacy questions and answers took effect Date</p>
+
+<p>Chapter will announce the next version by email. In the meantime, Chapter may update its contact information in these questions and answers by updating this page (). Chapter may change how it announces changes in a future version of these questions and answers.</p>
+
+<p>We use Amazon Web Services, Azure, and mLab for our servers and databases. You can read the privacy policy for Amazon Web Services, Microsoft Azure, and mLab.</p>
+
+<p>We use Stripe and PayPal to process donations. You can read the privacy policy for Stripe and for PayPal.</p>
+
+<p>We use the CloudFlare and Netlify Content Delivery Networks so that Chapter is fast in all parts of the world. You can read the privacy policy for CloudFlare and Netlify online.</p>
+
+<h2>When I delete my personal data from Chapter, is it really deleted from Chapter&aposs servers?</h2>
+<p>Yes. When you delete personal data from Chapter, we immediately delete it from our servers.</p>
+
+<p>We make emergency backups of our database every day, and we delete each of these backups after a few days. So within a few days, your personal data won&apost even exist in our backups.</p>
+
+<h2>Does Chapter meet Europe&aposs GDPR privacy regulations?</h2>
+<p>Yes. Chapter respects the privacy rights covered by Regulation (EU) 2016/679 - the European Union&aposs General Data Protection Regulation (GDPR). Our legal team has studied the GDPR, and we&aposve built new features to comply with all of its rules.</p>
+
+<p>Even though GDPR only protects European citizens, Chapter is extending these protections to everyone all over the world.</p>
+
+<p>Chapter has organized all the information that GDPR requires us to give you into this easy-to-read question-and-answer format. Our goal is to inform you of your specific rights to access data, erase it, port it, rectify it, and object to automated decision-making.</p>
+
+<h2>Who has access to my personal data?</h2>
+<p>Even though Chapter has thousands of volunteers, none of those people have access to your private data.</p>
+
+<p>Chapter has a few full-time staff, some of whom work directly on our databases. They have the ability to view your private data, but only do so when providing you with technical support.</p>
+
+<p>As for the personal data that you choose to share on your developer portfolio, anyone on the internet can see it by navigating to your developer portfolio&aposs public URL. Again, we&aposve given you full control over what parts of your developer profile are public.</p>
+*/}
+
+      <h2>What is Chapter&aposs Donor Privacy Policy?</h2>
+      <p>
+        Chapter will not share our donors&apos names or personal information
+        with anyone outside of our nonprofit organization&aposs team. Donors may
+        choose to display that they are donating to Chapter on their Chapter
+        profile. Otherwise, donor information will only be used to process
+        donations and send email confirmations. This policy applies to any
+        written, verbal, or electronic communication.
+      </p>
+
+      <h2>Can any other organizations access my data?</h2>
+      <p>
+        We don&apost sell your data to anyone. In order to provide service to
+        you, your data does pass through some other services. All of these
+        companies are based in the United States.
+      </p>
+
+      <p>
+        We use Auth0 to sign you into Chapter. You can read the privacy policy
+        for Auth0 online.
+      </p>
+
+      <p>
+        We use Google Analytics to help us understand the demographics of our
+        community and how people are using Chapter. You can opt out of Google
+        Analytics on Chapter by installing this browser plugin. You can read the
+        privacy policy for Google Analytics online. We also show Google ads in
+        some parts of the Chapter website.
+      </p>
+
+      <p>
+        That&aposs all, folks. Know your privacy rights, and stay safe out
+        there!
+      </p>
+    </div>
+  );
+};

68a4f15beec6423a6818f679c222e94ba3bb6c35
Sboonny <muhammed@freecodecamp.org>
2022-09-12 13:53:45 +0000

    HTML styling isn't working, revert to chakra basic styling

--- client/src/modules/policy/pages/policyPage.tsx
+++ client/src/modules/policy/pages/policyPage.tsx
@@ -1,91 +1,96 @@
 import React from 'react';
+import { Flex, Heading, Text } from '@chakra-ui/react';
 
 export const PolicyPage = () => {
   return (
-    <div>
-      <h1>
+    <Flex flexDirection={'column'} gap={'1em'}>
+      <Heading as={'h1'}>
         We take your privacy seriously. And we give you full control over your
         data.
-      </h1>
+      </Heading>
 
-      <h2>Does Chapter collect anonymous data?</h2>
-      <p>
+      <Heading as={'h2'}>Does Chapter collect anonymous data?</Heading>
+      <Text>
         When you use the Chapter website, we may collect some anonymous data so
         we can understand how people are using Chapter, and basic facts like
-        which browser they&aposre using.
-      </p>
+        which browser they&apos;re using.
+      </Text>
 
-      <h2>In what situations does Chapter collect personal data?</h2>
-      <p>
+      <Heading as={'h2'}>
+        In what situations does Chapter collect personal data?
+      </Heading>
+      <Text>
         If you create a Chapter account, we will collect some personal data so
         we can follow your progress toward earning developer certifications, and
         so you can customize your developer portfolio.
-      </p>
+      </Text>
 
-      <h2>Can I use Chapter anonymously?</h2>
-      <p>
-        Yes. You can access all of Chapter&aposs events and interactive Chapter
-        without creating an account. And if you don&apost create an account, we
-        won&apost collect any personal data about you.
-      </p>
+      <Heading as={'h2'}>Can I use Chapter anonymously?</Heading>
+      <Text>
+        Yes. You can access all of Chapter&apos;s events and interactive Chapter
+        without creating an account. And if you don&apos;t create an account, we
+        won&apos;t collect any personal data about you.
+      </Text>
 
-      <h2>If I create an account, what data will you collect?</h2>
-      <p>
-        We&aposll ask you for your email address so you can use it to sign into
+      <Heading as={'h2'}>
+        If I create an account, what data will you collect?
+      </Heading>
+      <Text>
+        We&apos;ll ask you for your email address so you can use it to sign into
         Chapter, and so we can provide...
-      </p>
+      </Text>
 
-      <p>
+      <Text>
         When you create an account on Chapter, we publish a organizer portfolio
         page for you on Chapter. If you want, you can add details about
         yourself, like your name, geographic location, and a link to your
         personal website.
-      </p>
+      </Text>
 
-      <p>
+      <Text>
         By default, your organizer portfolio will show which Chapter event you
         have organized.
-      </p>
+      </Text>
 
-      <p>
+      <Text>
         You have full control over your data, and can set any of these details
         to private, or delete them at any time.
-      </p>
+      </Text>
 
-      <h2>What is Chapter&aposs Donor Privacy Policy?</h2>
-      <p>
-        Chapter will not share our donors&apos names or personal information
-        with anyone outside of our nonprofit organization&aposs team. Donors may
-        choose to display that they are donating to Chapter on their Chapter
+      <Heading as={'h2'}>What is Chapter&apos;s Donor Privacy Policy?</Heading>
+      <Text>
+        Chapter will not share our donors&apos; names or personal information
+        with anyone outside of our nonprofit organization&apos;s team. Donors
+        may choose to display that they are donating to Chapter on their Chapter
         profile. Otherwise, donor information will only be used to process
         donations and send email confirmations. This policy applies to any
         written, verbal, or electronic communication.
-      </p>
+      </Text>
 
-      <h2>Can any other organizations access my data?</h2>
-      <p>
-        We don&apost sell your data to anyone. In order to provide service to
+      <Heading as={'h2'}>Can any other organizations access my data?</Heading>
+      <Text>
+        We don&apos;t sell your data to anyone. In order to provide service to
         you, your data does pass through some other services. All of these
         companies are based in the United States.
-      </p>
+      </Text>
 
-      <p>
+      <Text>
         We use Auth0 to sign you into Chapter. You can read the privacy policy
         for Auth0 online.
-      </p>
+      </Text>
 
-      <p>
+      <Text>
         We use Google Analytics to help us understand the demographics of our
         community and how people are using Chapter. You can opt out of Google
         Analytics on Chapter by installing this browser plugin. You can read the
         privacy policy for Google Analytics online. We also show Google ads in
         some parts of the Chapter website.
-      </p>
+      </Text>
 
-      <p>
-        That&aposs all, folks. Know your privacy rights, and stay safe out
+      <Text>
+        That&apos;s all, folks. Know your privacy rights, and stay safe out
         there!
-      </p>
-    </div>
+      </Text>
+    </Flex>
   );
 };

489b515f6957bd584df44f1b0bb37d79a0294687
Muhammed Mustafa <muhammed@freecodecamp.org>
2022-09-22 19:28:41 +0200

    chore: policy page (#1521)
    * add privacy navigation function
    
    Co-authored-by: Jon@1599 <55296387+Jonathan1599@users.noreply.github.com>

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -14,6 +14,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { ConfirmContextProvider } from 'chakra-confirm';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import PageLayout from '../components/PageLayout';
@@ -75,10 +76,14 @@ export const ConditionalWrap = (props: ConditionalWrapProps) => {
 
 const Auth0Wrapper = (children: React.ReactNode) => {
   // TODO: create a module to handle environment variables
+  const router = useRouter();
   const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN as string;
   const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID as string;
   const audience = process.env.NEXT_PUBLIC_AUTH0_AUDIENCE as string;
   const clientURL = process.env.NEXT_PUBLIC_CLIENT_URL as string;
+  const onRedirectCallback = () => {
+    router.replace('/policy');
+  };
   {
     /* TODO: Can we conditionally use window.location.origin for the redirectUri if
            we're in the browser? Or should we require site maintainers to supply it? */
@@ -88,6 +93,7 @@ const Auth0Wrapper = (children: React.ReactNode) => {
       domain={domain}
       clientId={clientId}
       redirectUri={clientURL}
+      onRedirectCallback={onRedirectCallback}
       audience={audience}
     >
       {children}

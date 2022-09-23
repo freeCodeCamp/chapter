320110e88622856d27d249a8281b4b801e6858c1
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-09-23 17:40:30 +0200

    fix: drop unused email input

--- server/src/controllers/User/input.ts
+++ server/src/controllers/User/input.ts
@@ -2,9 +2,7 @@ import { InputType, Field } from 'type-graphql';
 import { User } from '../../graphql-types';
 
 @InputType()
-export class UpdateUserInputs implements Omit<User, 'id'> {
+export class UpdateUserInputs implements Omit<User, 'id' | 'email'> {
   @Field(() => String, { nullable: true })
   name: string;
-  @Field(() => String, { nullable: true })
-  email: string;
 }

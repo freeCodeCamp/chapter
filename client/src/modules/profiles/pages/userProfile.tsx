ef835b2914f650c0e287cd3ce11b2f60cd42bd24
gikf <60067306+gikf@users.noreply.github.com>
2022-09-23 11:35:32 +0200

    feat: profile switch

--- client/src/modules/profiles/pages/userProfile.tsx
+++ client/src/modules/profiles/pages/userProfile.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Flex, Heading, Text, Link } from '@chakra-ui/layout';
+import React, { useEffect, useState } from 'react';
+import {
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Link,
+  Spinner,
+  Switch,
+  Text,
+} from '@chakra-ui/react';
 import { useConfirmDelete } from 'chakra-confirm';
 import { useRouter } from 'next/router';
 import { Button } from '@chakra-ui/button';
-import { useDeleteMeMutation } from '../../../generated/graphql';
+import {
+  useDeleteMeMutation,
+  useToggleAutoSubscribeMutation,
+} from '../../../generated/graphql';
 import { useAuthStore } from '../../auth/store';
+import { meQuery } from '../../auth/graphql/queries';
 
 export const UserProfilePage = () => {
   const {
@@ -12,15 +26,30 @@ export const UserProfilePage = () => {
     setData,
   } = useAuthStore();
   const router = useRouter();
+  const [autoSubscribe, setAutoSubscribe] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (user) setAutoSubscribe(user?.auto_subscribe);
+  }, [user]);
 
   const confirmDelete = useConfirmDelete({ doubleConfirm: true });
   const [deleteMe] = useDeleteMeMutation();
+  const [toggleAutoSubscribe] = useToggleAutoSubscribeMutation({
+    refetchQueries: [{ query: meQuery }],
+  });
   const clickDelete = async () => {
     const ok = await confirmDelete();
     if (!ok) return;
     deleteMe();
     setData({});
     router.push('/');
+  };
+  const onSwitch = async () => {
+    setLoading(true);
+    setAutoSubscribe(!autoSubscribe);
+    await toggleAutoSubscribe();
+    setLoading(false);
   };
 
   return (
@@ -33,6 +62,24 @@ export const UserProfilePage = () => {
           <Heading as="h2" size={'lg'}>
             Welcome {user.name}
           </Heading>
+          <FormControl>
+            <Flex>
+              <FormLabel htmlFor="subscription">
+                Automatic subscription
+              </FormLabel>
+              <Switch
+                id="subscription"
+                isChecked={autoSubscribe}
+                onChange={onSwitch}
+                isDisabled={isLoading}
+              />
+              {isLoading && <Spinner size="sm" />}
+            </Flex>
+            <FormHelperText>
+              Subscribe automatically to chapters that you join and RSVPed
+              events.
+            </FormHelperText>
+          </FormControl>
           {user.admined_chapters.length > 0 && (
             <>
               <Heading as="h2" marginBlock={'.5em'} size="md">
@@ -47,7 +94,6 @@ export const UserProfilePage = () => {
               </Flex>
             </>
           )}
-
           <Button colorScheme={'red'} marginBlock={'2em'} onClick={clickDelete}>
             Delete My Data
           </Button>

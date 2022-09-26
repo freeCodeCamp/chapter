8b739e88a8f7ad134ca5a77d98281ed2b7c470ac
gikf <60067306+gikf@users.noreply.github.com>
2022-09-26 20:40:12 +0200

    fix: incorrect interface

--- server/src/controllers/User/input.ts
+++ server/src/controllers/User/input.ts
@@ -2,7 +2,9 @@ import { InputType, Field } from 'type-graphql';
 import { User } from '../../graphql-types';
 
 @InputType()
-export class UpdateUserInputs implements Omit<User, 'id' | 'email'> {
+export class UpdateUserInputs
+  implements Omit<User, 'id' | 'email' | 'auto_subscribe'>
+{
   @Field(() => String, { nullable: true })
   name: string;
 }

8e48b01e5b974d89ab345c3c2b3fad5b88b0b68d
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-09-29 18:32:42 +0200

    fix: create RSVP table during init

--- server/prisma/generator/init.ts
+++ server/prisma/generator/init.ts
@@ -1,10 +1,12 @@
 import createChapterRoles from './factories/chapterRoles.factory';
 import createEventRoles from './factories/eventRoles.factory';
 import createInstanceRoles from './factories/instanceRoles.factory';
+import { createRsvpTypes } from './factories/rsvps.factory';
 
 const myArgs = process.argv.slice(2);
 if (myArgs.length === 1 && myArgs[0] === '--execute') {
   createRoles();
+  createRsvpTypes();
 } else if (myArgs.length === 0) {
   // do nothing, it's just being imported and will be called by the importing script
 } else {

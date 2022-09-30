6dd61964648162355368b0e47483053623441e77
Sboonny <muhammed@freecodecamp.org>
2022-09-30 12:18:24 +0000

    test the leave and join api only

--- cypress/e2e/chapters/[chapterId].cy.ts
+++ cypress/e2e/chapters/[chapterId].cy.ts
@@ -8,9 +8,6 @@ describe('chapter page', () => {
   });
 
   it('user can join chapter and change subscription status', () => {
-    cy.login('test@user.org');
-    cy.visit(`/chapters/${chapterId}`);
-
     cy.joinChapter(chapterId, { withAuth: true }).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body.errors).not.to.exist;
@@ -22,15 +19,6 @@ describe('chapter page', () => {
         expect(response.body.errors).not.to.exist;
       },
     );
-
-    cy.getChapterMembers(chapterId).then((chapter_users) => {
-      expect(
-        chapter_users.findIndex(
-          ({ user: { email }, subscribed }) =>
-            email === 'test@user.org' && subscribed,
-        ),
-      ).to.not.equal(-1);
-    });
   });
 
   it('should reject joining and subscribing requests from non-members', () => {
@@ -41,9 +29,6 @@ describe('chapter page', () => {
   });
 
   it('user can leave chapter', () => {
-    cy.login('test@user.org');
-    cy.visit(`/chapters/${chapterId}`);
-
     cy.leaveChapter(chapterId, { withAuth: true }).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body.errors).not.to.exist;
@@ -51,6 +36,6 @@ describe('chapter page', () => {
   });
 
   it('should reject leaving chapter from non-members', () => {
-    cy.joinChapter(chapterId, { withAuth: false }).then(expectToBeRejected);
+    cy.leaveChapter(chapterId, { withAuth: false }).then(expectToBeRejected);
   });
 });

3e96f4af7cc32c2a5294e5b7beab1f74eb856482
Sboonny <muhammed@freecodecamp.org>
2022-10-01 08:44:05 +0000

    Refactor cypress join and leave test

--- cypress/e2e/chapters/[chapterId].cy.ts
+++ cypress/e2e/chapters/[chapterId].cy.ts
@@ -7,18 +7,24 @@ describe('chapter page', () => {
     cy.task('seedDb');
   });
 
+  //ToDo: check for if UI components are functioning as intended
   it('user can join chapter and change subscription status', () => {
+    cy.login();
+
     cy.joinChapter(chapterId, { withAuth: true }).then((response) => {
       expect(response.status).to.eq(200);
       expect(response.body.errors).not.to.exist;
     });
-
     cy.toggleChapterSubscription(chapterId, { withAuth: true }).then(
       (response) => {
         expect(response.status).to.eq(200);
         expect(response.body.errors).not.to.exist;
       },
     );
+    cy.leaveChapter(chapterId, { withAuth: true }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.errors).not.to.exist;
+    });
   });
 
   it('should reject joining and subscribing requests from non-members', () => {
@@ -26,13 +32,6 @@ describe('chapter page', () => {
     cy.toggleChapterSubscription(chapterId, { withAuth: false }).then(
       expectToBeRejected,
     );
-  });
-
-  it('user can leave chapter', () => {
-    cy.leaveChapter(chapterId, { withAuth: true }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.errors).not.to.exist;
-    });
   });
 
   it('should reject leaving chapter from non-members', () => {

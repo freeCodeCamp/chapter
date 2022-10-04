3e42108871d1906c67550a0e89ccb1e33df22dae
Krzysztof G <60067306+gikf@users.noreply.github.com>
2022-10-04 14:45:08 +0200

    feat: change permission for displaying edit sponsor form (#1641)

--- client/src/modules/dashboard/Sponsors/pages/EditSponsorPage.tsx
+++ client/src/modules/dashboard/Sponsors/pages/EditSponsorPage.tsx
@@ -1,32 +1,40 @@
 import { NextPage } from 'next';
 import NextError from 'next/error';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { useParam } from '../../../../hooks/useParam';
 import { Sponsors } from '../../Events/graphql/queries';
 import { Layout } from '../../shared/components/Layout';
 import SponsorForm, { SponsorFormData } from '../components/SponsorForm';
-import { SPONSOR } from '../graphql/queries';
+import { DASHBOARD_SPONSOR } from '../graphql/queries';
 import { DashboardLoading } from '../../shared/components/DashboardLoading';
-import { useSponsorQuery, useUpdateSponsorMutation } from 'generated/graphql';
+import {
+  useDashboardSponsorLazyQuery,
+  useUpdateSponsorMutation,
+} from 'generated/graphql';
 
 const EditSponsorPage: NextPage = () => {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
   const { param: sponsorId, isReady } = useParam('id');
-  const {
-    loading: sponsorLoading,
-    error,
-    data,
-  } = useSponsorQuery({
-    variables: { sponsorId },
-  });
+  const [getSponsor, { loading: sponsorLoading, error, data }] =
+    useDashboardSponsorLazyQuery({
+      variables: { sponsorId },
+    });
   const [updateSponsor] = useUpdateSponsorMutation({
     refetchQueries: [
-      { query: SPONSOR, variables: { id: sponsorId } },
+      { query: DASHBOARD_SPONSOR, variables: { id: sponsorId } },
       { query: Sponsors },
     ],
   });
+
+  useEffect(() => {
+    if (isReady) {
+      getSponsor();
+    }
+  }, [isReady]);
+
   const onSubmit = async (data: SponsorFormData) => {
     setLoading(true);
     try {
@@ -47,7 +55,7 @@ const EditSponsorPage: NextPage = () => {
   const isLoading = sponsorLoading || !isReady || !data;
   if (isLoading || error)
     return <DashboardLoading loading={isLoading} error={error} />;
-  if (!data.sponsor)
+  if (!data.dashboardSponsor)
     return <NextError statusCode={404} title="Sponsor not found" />;
 
   return (

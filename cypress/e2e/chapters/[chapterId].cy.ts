dd74c34689d1bdbcdca8960b6e6f36749d095985
Sboonny <muhammed@freecodecamp.org>
2022-10-04 05:02:31 +0000

    Cypress: Owner shouldn't be able to join or leave chapter

--- cypress/e2e/chapters/[chapterId].cy.ts
+++ cypress/e2e/chapters/[chapterId].cy.ts
@@ -7,9 +7,13 @@ describe('chapter page', () => {
     cy.task('seedDb');
   });
 
-  //ToDo: check for if UI components are functioning as intended
-  it('user can join chapter and change subscription status', () => {
-    cy.login();
+  //ToDo: check UI components are functioning as intended
+  // the UI components of Chapter page haven't been set on stone, yet
+  // And similar function will be added to profile for qulity of life
+  // When both function set on stone, we can add guardrails to check
+  // if nothing is breaking
+  it('user can join, leave chapter, and change subscription status', () => {
+    cy.login('test@user.org');
 
     cy.joinChapter(chapterId, { withAuth: true }).then((response) => {
       expect(response.status).to.eq(200);
@@ -25,6 +29,18 @@ describe('chapter page', () => {
       expect(response.status).to.eq(200);
       expect(response.body.errors).not.to.exist;
     });
+  });
+
+  it('Owner can not join and leave chapter', () => {
+    cy.login();
+
+    cy.joinChapter(chapterId, { withAuth: true }).then(expectToBeRejected);
+  });
+
+  it('Owner can not join and leave chapter', () => {
+    cy.login();
+
+    cy.leaveChapter(chapterId, { withAuth: true }).then(expectToBeRejected);
   });
 
   it('should reject joining and subscribing requests from non-members', () => {

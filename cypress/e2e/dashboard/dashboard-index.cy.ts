659f212f3986284f28de84987c4f333d9bd6666c
Krzysztof G <60067306+gikf@users.noreply.github.com>
2022-10-06 15:23:32 +0200

    feat: ability to wait for client permission check (#1659)
    * feat: ability to wait for client permission check
    
    * test: update available dashboard tabs
    
    * test: fix selecting chapters link
    
    * refactor: have useAuth track loading status
    
    * refactor: move and rename useCheckPermission
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- cypress/e2e/dashboard/dashboard-index.cy.ts
+++ cypress/e2e/dashboard/dashboard-index.cy.ts
@@ -1,9 +1,35 @@
 describe('dashboard', () => {
-  it('should have links to the dashboards', () => {
+  before(() => {
+    cy.task('seedDb');
     cy.visit('/dashboard');
+  });
+
+  it('instance owner should have links to all the dashboard', () => {
+    cy.login();
     cy.get('a[href="/dashboard/events"]').should('be.visible');
     cy.get('a[href="/dashboard/chapters"]').should('be.visible');
     cy.get('a[href="/dashboard/venues"]').should('be.visible');
     cy.get('a[href="/dashboard/users"]').should('be.visible');
+    cy.get('a[href="/dashboard/sponsors"]').should('be.visible');
+  });
+
+  it('chapter administrator should have links to the dashboards allowed for chapter administrator', () => {
+    cy.login('admin@of.chapter.one');
+    cy.get('a[href="/dashboard/events"]').should('be.visible');
+    cy.get('a[href="/dashboard/chapters"]').should('be.visible');
+    cy.get('a[href="/dashboard/venues"]').should('be.visible');
+    cy.get('a[href="/dashboard/sponsors"]').should('be.visible');
+
+    cy.get('a[href="/dashboard/users"]').should('not.exist');
+  });
+
+  it('member should have links to the dashboards allowed for member', () => {
+    cy.login('test@user.org');
+    cy.get('a[href="/dashboard/events"]').should('be.visible');
+    cy.get('a[href="/dashboard/chapters"]').should('be.visible');
+    cy.get('a[href="/dashboard/venues"]').should('be.visible');
+
+    cy.get('a[href="/dashboard/sponsors"]').should('not.exist');
+    cy.get('a[href="/dashboard/users"]').should('not.exist');
   });
 });

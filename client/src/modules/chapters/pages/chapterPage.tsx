f7bd208506e53cf378b4f63b162cf0707dd6faf8
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-10-07 18:40:57 +0200

    refactor: chapter page (#1675)
    * refactor: split ChapterPage into sub components
    
    * refactor: drop unused HStack
    
    * feat: return isLoggedIn from auth/store
    
    * refactor: use isLoggedIn

--- client/src/modules/chapters/pages/chapterPage.tsx
+++ client/src/modules/chapters/pages/chapterPage.tsx
@@ -25,12 +25,13 @@ import {
   useChapterUserLazyQuery,
   useJoinChapterMutation,
   useToggleChapterSubscriptionMutation,
+  ChapterUserQuery,
 } from 'generated/graphql';
 import { useParam } from 'hooks/useParam';
 
 export const ChapterPage: NextPage = () => {
   const { param: chapterId, isReady } = useParam('chapterId');
-  const { user } = useAuth();
+  const { isLoggedIn } = useAuth();
 
   const [getChapter, { loading, error, data }] = useChapterLazyQuery({
     variables: { chapterId },
@@ -133,45 +134,20 @@ export const ChapterPage: NextPage = () => {
         <Text fontSize={'lg'} color={'gray.500'}>
           {data.chapter.description}
         </Text>
-        {user &&
+        {isLoggedIn &&
           (loadingChapterUser ? (
             <Spinner />
           ) : dataChapterUser ? (
-            <HStack>
-              {dataChapterUser.chapterUser.subscribed ? (
-                <HStack>
-                  <CheckIcon />
-                  <Text>
-                    {dataChapterUser.chapterUser.chapter_role.name} of the
-                    chapter
-                  </Text>
-                  <Button onClick={() => chapterSubscribe(false)} size="md">
-                    Unsubscribe
-                  </Button>
-                </HStack>
-              ) : (
-                <Button
-                  colorScheme="blue"
-                  onClick={() => chapterSubscribe(true)}
-                  size="md"
-                >
-                  Subscribe
-                </Button>
-              )}
-            </HStack>
+            <SubscriptionWidget
+              chapterUser={dataChapterUser.chapterUser}
+              chapterSubscribe={chapterSubscribe}
+            />
           ) : (
             <Button colorScheme="blue" onClick={joinChapter}>
               Join chapter
             </Button>
           ))}
-        {data.chapter.chat_url && (
-          <div>
-            <Heading size="md" color={'gray.700'}>
-              Chat Link:
-            </Heading>
-            <Link>{data.chapter.chat_url}</Link>
-          </div>
-        )}
+        <ChatLink chatUrl={data.chapter.chat_url} />
         <Heading size="md" color={'gray.700'}>
           Events:
         </Heading>
@@ -180,12 +156,44 @@ export const ChapterPage: NextPage = () => {
             key={event.id}
             event={{
               ...event,
-              // Fix this | undefined
-              chapter: { id: chapterId, name: data.chapter?.name || '' },
+              chapter: { id: chapterId, name: data.chapter.name },
             }}
           />
         ))}
       </Stack>
     </VStack>
+  );
+};
+
+const ChatLink = ({ chatUrl }: { chatUrl?: string | null }) => {
+  return chatUrl ? (
+    <div>
+      <Heading size="md" color={'gray.700'}>
+        Chat Link:
+      </Heading>
+      <Link>{chatUrl}</Link>
+    </div>
+  ) : null;
+};
+
+const SubscriptionWidget = ({
+  chapterUser,
+  chapterSubscribe,
+}: {
+  chapterUser: ChapterUserQuery['chapterUser'];
+  chapterSubscribe: (toSubscribe: boolean) => Promise<void>;
+}) => {
+  return chapterUser.subscribed ? (
+    <HStack>
+      <CheckIcon />
+      <Text>{chapterUser.chapter_role.name} of the chapter</Text>
+      <Button onClick={() => chapterSubscribe(false)} size="md">
+        Unsubscribe
+      </Button>
+    </HStack>
+  ) : (
+    <Button colorScheme="blue" onClick={() => chapterSubscribe(true)} size="md">
+      Subscribe
+    </Button>
   );
 };

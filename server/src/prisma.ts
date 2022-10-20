108c70b181bb987e4f1977bfe9f8b3413672fdeb
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-10-20 13:05:33 +0200

    refactor: explain where magic constants come from

--- server/src/prisma.ts
+++ server/src/prisma.ts
@@ -5,5 +5,6 @@ import './config';
 
 export const prisma = new PrismaClient();
 
+/* Prisma error codes: https://www.prisma.io/docs/reference/api-reference/error-reference#error-codes */
 export const RECORD_MISSING = 'P2025';
 export const UNIQUE_CONSTRAINT_FAILED = 'P2002';

54eff615fd57d7fccba329d8a13a61933365c248
gikf <60067306+gikf@users.noreply.github.com>
2022-11-12 21:49:15 +0100

    feat: reset apollo store on logout

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,3 +1,4 @@
+import { useApolloClient } from '@apollo/client';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useMeQuery } from 'generated/graphql';
 import { useSession } from 'hooks/useSession';
@@ -22,12 +23,12 @@ export const useLogin = () => {
 export const useLogout = () => {
   const { logout: logoutAuth0 } = useAuth0();
   const { destroySession } = useSession();
-  const { refetch } = useMeQuery();
+  const client = useApolloClient();
 
   const logout = async () => {
     if (process.env.NEXT_PUBLIC_USE_AUTH0 !== 'false') logoutAuth0();
     await destroySession();
-    return await refetch();
+    return await client.resetStore();
   };
 
   return logout;

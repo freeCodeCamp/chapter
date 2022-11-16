5cdc8998dc24d8e2759b17581bc4b178e7007e5e
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-11-16 12:24:02 +0100

    fix: add new role and update role permissions

--- server/prisma/migrations/20221116090943_update_permissions/migration.sql
+++ server/prisma/migrations/20221116090943_update_permissions/migration.sql
@@ -14,3 +14,44 @@ UPDATE instance_permissions SET name = 'event-subscription-manage' WHERE name = 
 
 UPDATE chapter_permissions SET name = 'event-subscription-manage' WHERE name = 'event-subscriptions-manage';
 
+-- Add new instance permission (sponsor-view) to owner role
+
+INSERT INTO instance_role_permissions (instance_role_id, instance_permission_id, created_at, updated_at)
+VALUES (
+  (SELECT id FROM instance_roles WHERE name = 'owner'),
+  (SELECT id FROM instance_permissions WHERE name = 'sponsor-view'),
+  NOW(),
+  NOW()
+) ON CONFLICT DO NOTHING;
+
+-- Add new instance role (chapter_administrator)
+
+INSERT INTO instance_roles (name, created_at, updated_at)
+VALUES ('chapter_administrator', NOW(), NOW())
+ON CONFLICT DO NOTHING;
+
+-- Add permissions (chapter-join, chapter-subscription-manage, sponsor-view) for new instance role (chapter_administrator)
+
+INSERT INTO instance_role_permissions (instance_role_id, instance_permission_id, created_at, updated_at)
+VALUES (
+  (SELECT id FROM instance_roles WHERE name = 'chapter_administrator'),
+  (SELECT id FROM instance_permissions WHERE name = 'chapter-subscription-manage'),
+  NOW(),
+  NOW()
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO instance_role_permissions (instance_role_id, instance_permission_id, created_at, updated_at)
+VALUES (
+  (SELECT id FROM instance_roles WHERE name = 'chapter_administrator'),
+  (SELECT id FROM instance_permissions WHERE name = 'chapter-join'),
+  NOW(),
+  NOW()
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO instance_role_permissions (instance_role_id, instance_permission_id, created_at, updated_at)
+VALUES (
+  (SELECT id FROM instance_roles WHERE name = 'chapter_administrator'),
+  (SELECT id FROM instance_permissions WHERE name = 'sponsor-view'),
+  NOW(),
+  NOW()
+) ON CONFLICT DO NOTHING;

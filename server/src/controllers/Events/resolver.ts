f5a2d5f1caef0ee14ac0464c775672f0888eab80
Krzysztof G <60067306+gikf@users.noreply.github.com>
2022-11-17 14:17:09 +0100

    fix: url param name (#1913)

--- server/src/controllers/Events/resolver.ts
+++ server/src/controllers/Events/resolver.ts
@@ -962,7 +962,7 @@ ${unsubscribeOptions}`,
     const subject = `Invitation to ${event.name}.`;
 
     const chapterURL = `${process.env.CLIENT_LOCATION}/chapters/${event.chapter.id}`;
-    const eventURL = `${process.env.CLIENT_LOCATION}/events/${event.id}?emaillink=true`;
+    const eventURL = `${process.env.CLIENT_LOCATION}/events/${event.id}?ask_to_confirm=true`;
     const calendar = ical();
     calendar.createEvent({
       start: event.start_at,

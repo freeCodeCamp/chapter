71f955833202ad2ff9c0d7c7d2bde12750ca1656
Sboonny <muhammed@freecodecamp.org>
2022-11-23 09:53:51 +0000

    WIP/grid-layout-in-the-footer

---
+++ client/src/components/PageLayout/component/Footer.tsx
@@ -0,0 +1,60 @@
+import { Box, Grid, Heading, ListItem, UnorderedList } from '@chakra-ui/layout';
+import React from 'react';
+import { LinkButton } from 'chakra-next-link';
+
+export const Footer: React.FC = () => {
+  return (
+    <Grid
+      marginTop="2em"
+      backgroundColor={'gray.10'}
+      paddingInline="2em"
+      paddingBlock="1em"
+      gap=".5em"
+      gridTemplateColumns={{ base: 'none', lg: 'repeat(2, 1fr)' }}
+      justifyItems={{ base: 'center', lg: 'flex-start' }}
+      templateAreas={{
+        base: `
+          "chapterpolicy"
+          "policy"
+          "policylink"
+          `,
+        lg: `
+          "chapterpolicy policyLink"
+          "policy policy"
+          `,
+      }}
+    >
+      <Heading gridArea="chapterpolicy" as="h2">
+        Chapter Policy
+      </Heading>
+      <UnorderedList gridArea="policy" marginInline="1.5em" spacing={1}>
+        <ListItem>
+          We are storing your email and linking it to RSVP system, but we are
+          the only one with access to it.
+        </ListItem>
+        <ListItem>
+          We are trying our best to store the least amount of data about your
+          usage in our database.
+        </ListItem>
+        <ListItem>
+          You can delete your data at anytime and it will be removed from our
+          database in your profile
+        </ListItem>
+      </UnorderedList>
+      <Box>
+        <LinkButton
+          gridArea="policyLink"
+          href="/policy"
+          fontWeight="600"
+          background={'gray.85'}
+          color={'gray.10'}
+          height={'2.5em'}
+          borderRadius={'5px'}
+          _hover={{ color: 'gray.85', backgroundColor: 'gray.10' }}
+        >
+          Know More
+        </LinkButton>
+      </Box>
+    </Grid>
+  );
+};

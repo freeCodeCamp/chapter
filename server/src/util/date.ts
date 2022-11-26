7bce3ab51e2ac4a7223e962e4216320265e41fcb
Sboonny <muhammed@freecodecamp.org>
2022-11-26 13:10:49 +0000

    Add time zone in email format

--- server/src/util/date.ts
+++ server/src/util/date.ts
@@ -1,5 +1,5 @@
 import { format } from 'date-fns';
 
 export const formatDate = (date: Date) => {
-  return format(new Date(date), 'E, LLL d @ HH:mm');
+  return format(new Date(date), 'E, LLL d @ HH:mm OOOO');
 };

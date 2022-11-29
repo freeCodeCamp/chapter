53802eb3a30695611f4500d3550a9ae079ff4ca6
Sboonny <muhammed@freecodecamp.org>
2022-11-29 09:47:21 +0000

    Add comment about NextLink and remove extra divs

--- client/src/components/PageLayout/Header.tsx
+++ client/src/components/PageLayout/Header.tsx
@@ -4,7 +4,6 @@ import {
   Box,
   Image,
   Button,
-  Flex,
   Menu,
   MenuList,
   MenuItem,
@@ -53,78 +52,77 @@ export const Header: React.FC = () => {
           />
         </Link>
         <HStack as="nav">
-          <Box>
-            {loadingUser ? (
-              <Spinner color="white" size="xl" />
-            ) : (
-              <Menu>
-                <MenuButton
-                  as={Button}
-                  data-cy="menu-button"
-                  aria-label="Options"
-                  variant="outline"
-                  background="gray.10"
-                  px={[2, 4]}
-                  py={[1, 2]}
-                >
-                  Menu
-                </MenuButton>
-                <MenuList paddingBlock={0}>
-                  <Flex
-                    flexDirection="column"
+          {loadingUser ? (
+            <Spinner color="white" size="xl" />
+          ) : (
+            <Menu>
+              <MenuButton
+                as={Button}
+                data-cy="menu-button"
+                aria-label="Options"
+                variant="outline"
+                background="gray.10"
+                px={[2, 4]}
+                py={[1, 2]}
+              >
+                Menu
+              </MenuButton>
+              <MenuList
+                paddingBlock={0}
+                display="flex"
+                flexDirection="column"
+                fontWeight="600"
+                borderRadius="5px"
+              >
+                {/* We are using Nextlink because of confustion in NextJs which harm the loading functionality and force the page to wait for the JS loading to display it */}
+                <NextLink passHref href="/chapters">
+                  <MenuItem as="a">Chapters</MenuItem>
+                </NextLink>
+
+                <NextLink passHref href="/events">
+                  <MenuItem as="a">Events</MenuItem>
+                </NextLink>
+
+                {user && (
+                  <Box borderBlock={'1px'} borderColor={'gray.85'}>
+                    <NextLink passHref href="/profile">
+                      <MenuItem as="a">Profile</MenuItem>
+                    </NextLink>
+
+                    {checkPermission(user, Permission.ChaptersView) && (
+                      <NextLink passHref href="/dashboard/chapters">
+                        <MenuItem data-cy="menu-dashboard-link" as="a">
+                          Dashboard
+                        </MenuItem>
+                      </NextLink>
+                    )}
+                  </Box>
+                )}
+                {user ? (
+                  <MenuItem
+                    data-cy="logout-button"
+                    onClick={() => logout().then(goHome)}
                     fontWeight="600"
-                    borderRadius="5px"
+                    height={'100%'}
                   >
-                    <NextLink passHref href="/chapters">
-                      <MenuItem as="a">Chapters</MenuItem>
-                    </NextLink>
-
-                    <NextLink passHref href="/events">
-                      <MenuItem as="a">Events</MenuItem>
-                    </NextLink>
-
-                    {user && (
-                      <Box borderBlock={'1px'} borderColor={'gray.85'}>
-                        <NextLink passHref href="/profile">
-                          <MenuItem as="a">Profile</MenuItem>
-                        </NextLink>
-
-                        {checkPermission(user, Permission.ChaptersView) && (
-                          <NextLink passHref href="/dashboard/chapters">
-                            <MenuItem data-cy="menu-dashboard-link" as="a">
-                              Dashboard
-                            </MenuItem>
-                          </NextLink>
-                        )}
-                      </Box>
-                    )}
-                    {user ? (
-                      <MenuItem
-                        data-cy="logout-button"
-                        onClick={() => logout().then(goHome)}
-                        fontWeight="600"
-                        height={'100%'}
-                      >
-                        Logout
-                      </MenuItem>
-                    ) : (
-                      <MenuItem
-                        data-cy="login-button"
-                        onClick={login}
-                        fontWeight="600"
-                        height={'100%'}
-                      >
-                        Login
-                      </MenuItem>
-                    )}
-                  </Flex>
-                </MenuList>
-              </Menu>
-            )}
-          </Box>
+                    Logout
+                  </MenuItem>
+                ) : (
+                  <MenuItem
+                    data-cy="login-button"
+                    onClick={login}
+                    fontWeight="600"
+                    height={'100%'}
+                  >
+                    Login
+                  </MenuItem>
+                )}
+              </MenuList>
+            </Menu>
+          )}
           {user && (
             <NextLink passHref href="/profile">
-              <Avatar user={user} cursor="pointer" />
+              <Avatar user={user} cursor="pointer" title="Profile" />
             </NextLink>
           )}
         </HStack>

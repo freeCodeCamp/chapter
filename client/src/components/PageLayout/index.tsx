cfdf3ea30d60430e500635ab6b8a859520270c03
Sboonny <muhammed@freecodecamp.org>
2022-12-01 04:23:21 +0000

    remove extra white spaces

--- client/src/components/PageLayout/index.tsx
+++ client/src/components/PageLayout/index.tsx
@@ -10,28 +10,28 @@ const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
-        <Alert status="error" marginBlock={'1em'}>
-          <AlertIcon />
-          <AlertDescription>
-            This is a testing site for freeCodeCamp staff members and Chapter
-            maintainers. Be mindful that your data will be deleted periodically.
-            <LinkButton
-              href="/policy"
-              fontWeight="600"
-              marginTop={'.25em'}
-              background={'gray.85'}
-              color={'gray.10'}
-              height={'2.5em'}
-              borderRadius={'5px'}
-              _hover={{
-                color: 'gray.85',
-                backgroundColor: 'gray.10',
-              }}
-            >
-              Know more about our policy
-            </LinkButton>
-          </AlertDescription>
-        </Alert>
+      <Alert status="error" marginBlock={'1em'}>
+        <AlertIcon />
+        <AlertDescription>
+          This is a testing site for freeCodeCamp staff members and Chapter
+          maintainers. Be mindful that your data will be deleted periodically.
+          <LinkButton
+            href="/policy"
+            fontWeight="600"
+            marginTop={'.25em'}
+            background={'gray.85'}
+            color={'gray.10'}
+            height={'2.5em'}
+            borderRadius={'5px'}
+            _hover={{
+              color: 'gray.85',
+              backgroundColor: 'gray.10',
+            }}
+          >
+            Know more about our policy
+          </LinkButton>
+        </AlertDescription>
+      </Alert>
       <SkipNavContent />
       <Box px={[4, 4, 8, 16]} id="main-content">
         {children}

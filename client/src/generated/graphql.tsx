d5049a81c09506ebe23383d58a7f8b89733629e2
Krzysztof G <60067306+gikf@users.noreply.github.com>
2022-12-01 09:19:19 +0100

    chore: remove not needed code from resolver (#2003)

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -344,7 +344,7 @@ export type Mutation = {
   deleteRsvp: Scalars['Boolean'];
   deleteVenue: Venue;
   joinChapter: ChapterUserWithRole;
-  leaveChapter: ChapterUserWithRole;
+  leaveChapter: ChapterUser;
   rsvpEvent: EventUserWithRelations;
   sendEmail: Email;
   sendEventInvite: Scalars['Boolean'];
@@ -509,7 +509,6 @@ export type Query = {
   chapter: ChapterWithRelations;
   chapterRoles: Array<ChapterRole>;
   chapterUser?: Maybe<ChapterUserWithRelations>;
-  chapterUsers: Array<ChapterUser>;
   chapterVenues: Array<Venue>;
   chapters: Array<ChapterCardRelations>;
   dashboardChapter: ChapterWithRelations;
@@ -539,10 +538,6 @@ export type QueryChapterArgs = {
 
 export type QueryChapterUserArgs = {
   chapterId: Scalars['Int'];
-};
-
-export type QueryChapterUsersArgs = {
-  id: Scalars['Int'];
 };
 
 export type QueryChapterVenuesArgs = {
@@ -829,10 +824,7 @@ export type LeaveChapterMutationVariables = Exact<{
 
 export type LeaveChapterMutation = {
   __typename?: 'Mutation';
-  leaveChapter: {
-    __typename?: 'ChapterUserWithRole';
-    chapter_role: { __typename?: 'ChapterRole'; name: string };
-  };
+  leaveChapter: { __typename?: 'ChapterUser'; user_id: number };
 };
 
 export type ToggleChapterSubscriptionMutationVariables = Exact<{
@@ -1943,9 +1935,7 @@ export type JoinChapterMutationOptions = Apollo.BaseMutationOptions<
 export const LeaveChapterDocument = gql`
   mutation leaveChapter($chapterId: Int!) {
     leaveChapter(chapterId: $chapterId) {
-      chapter_role {
-        name
-      }
+      user_id
     }
   }
 `;

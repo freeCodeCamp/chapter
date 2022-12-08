81b0a01333264da4e3c91f32f56e088b4a293a58
Muhammed Mustafa <muhammed@freecodecamp.org>
2022-12-08 16:25:37 +0200

    feat: use semantic html and update footer content (#2064)
    Co-authored-by: gikf <60067306+gikf@users.noreply.github.com>
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- client/src/components/PageLayout/index.tsx
+++ client/src/components/PageLayout/index.tsx
@@ -51,7 +51,12 @@ const PageLayout = ({ children }: { children: React.ReactNode }) => {
           </Alert>
         )}
       <SkipNavContent />
-      <Box px={[4, 4, 8, 16]} id="main-content">
+      <Box
+        as="main"
+        minHeight={{ base: '70vh', '2xl': '82vh' }}
+        px={[4, 4, 8, 16]}
+        id="main-content"
+      >
         {children}
       </Box>
       <Footer />

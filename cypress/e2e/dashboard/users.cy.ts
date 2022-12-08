697ce900307e3cbb7f0fe5040400bc64546ea0f5
Muhammed Mustafa <MuhammedElruby@gmail.com>
2022-12-08 10:35:15 +0200

    Use memberToOwnerToMember name in the cypress test

--- cypress/e2e/dashboard/users.cy.ts
+++ cypress/e2e/dashboard/users.cy.ts
@@ -55,7 +55,7 @@ describe('Users dashboard', () => {
         .mhGetBody()
         .should(
           'include',
-          `Hello, ${memberRole.name}<br />
+          `Hello, ${memberToOwnerToMember.name}<br />
         This is a notification about your role in chapter has changed to ${memberRole}.`,
         );
 

51d74b43fbbe633486a7b15eaed085de6a4e768c
Krzysztof G <60067306+gikf@users.noreply.github.com>
2022-12-18 20:10:36 +0100

    fix: remove no longer used argument (#2114)

--- cypress/e2e/dashboard/events/events-index.cy.ts
+++ cypress/e2e/dashboard/events/events-index.cy.ts
@@ -251,14 +251,14 @@ describe('events dashboard', () => {
 
   it('chapter admin should be allowed to send email to attendees', () => {
     const eventId = 1;
-    cy.sendEventInvite(eventId, ['confirmed']).then(expectNoErrors);
+    cy.sendEventInvite(eventId).then(expectNoErrors);
 
     cy.login(users.testUser.email);
-    cy.sendEventInvite(eventId, ['confirmed']).then(expectToBeRejected);
+    cy.sendEventInvite(eventId).then(expectToBeRejected);
 
     // banned admin should be rejected
     cy.login(users.bannedAdmin.email);
-    cy.sendEventInvite(eventId, ['confirmed']).then(expectToBeRejected);
+    cy.sendEventInvite(eventId).then(expectToBeRejected);
   });
 
   it('chapter admin should see only events from admined chapters', () => {

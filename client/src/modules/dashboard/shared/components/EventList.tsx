6a14d1697b9531de5ac884f62783a16881695edb
Sboonny <muhammed@freecodecamp.org>
2022-12-28 11:06:15 +0000

    Remove gridItem and swap heading with Text element

--- client/src/modules/dashboard/shared/components/EventList.tsx
+++ client/src/modules/dashboard/shared/components/EventList.tsx
@@ -1,4 +1,4 @@
-import { Flex, Grid, GridItem, Heading, Tag } from '@chakra-ui/react';
+import { Flex, Grid, Heading, Tag, Text } from '@chakra-ui/react';
 import React from 'react';
 
 import { LinkButton } from 'chakra-next-link';
@@ -25,48 +25,41 @@ export const EventList = ({ events, emptyText, title }: Props) => {
       {events.length > 0 ? (
         <Grid gap="2em">
           {events.map(({ canceled, id, invite_only, name }) => (
-            <GridItem key={id}>
-              <Flex justifyContent="space-between">
-                <LinkButton href={`/events/${id}`}>{name}</LinkButton>
-                <Flex>
-                  {canceled && (
-                    <Tag
-                      borderRadius="lg"
-                      marginRight="3"
-                      paddingInline="[1 , 2]"
-                      paddingBlock="[.5, 1]"
-                      fontSize="['small', 'md']"
-                      maxWidth="8em"
-                      mt="1"
-                      maxH="2em"
-                      colorScheme="red"
-                    >
-                      Canceled
-                    </Tag>
-                  )}
-                  {invite_only && (
-                    <Tag
-                      borderRadius="lg"
-                      mt="1"
-                      paddingInline="[1 , 2]"
-                      paddingBlock="[.5, 1]"
-                      colorScheme="blue"
-                      fontSize="['small', 'md']"
-                      maxWidth="8em"
-                      maxH="2em"
-                    >
-                      Invite only
-                    </Tag>
-                  )}
-                </Flex>
+            <Flex justifyContent="space-between" key={id}>
+              <LinkButton href={`/events/${id}`}>{name}</LinkButton>
+              <Flex marginTop="1" gap="1em">
+                {invite_only && (
+                  <Tag
+                    borderRadius="lg"
+                    paddingInline="[1 , 2]"
+                    paddingBlock="[.5, 1]"
+                    colorScheme="blue"
+                    fontSize="['small', 'md']"
+                    maxWidth="8em"
+                    maxH="2em"
+                  >
+                    Invite only
+                  </Tag>
+                )}
+                {canceled && (
+                  <Tag
+                    borderRadius="lg"
+                    paddingInline="[1 , 2]"
+                    paddingBlock="[.5, 1]"
+                    fontSize="['small', 'md']"
+                    maxWidth="8em"
+                    maxH="2em"
+                    colorScheme="red"
+                  >
+                    Canceled
+                  </Tag>
+                )}
               </Flex>
-            </GridItem>
+            </Flex>
           ))}
         </Grid>
       ) : (
-        <Heading as="h3" size="sm">
-          {emptyText || 'No events'}
-        </Heading>
+        <Text>{emptyText || 'No events'}</Text>
       )}
     </>
   );

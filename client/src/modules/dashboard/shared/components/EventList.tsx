daf26e9f3e07ee5385acc4d3ae55946f755a226e
gikf <60067306+gikf@users.noreply.github.com>
2022-12-28 11:07:47 +0100

    feat: add message for no events

--- client/src/modules/dashboard/shared/components/EventList.tsx
+++ client/src/modules/dashboard/shared/components/EventList.tsx
@@ -12,55 +12,62 @@ interface Event {
 
 interface Props {
   events: Event[];
+  emptyText?: string;
   title: string;
 }
 
-export const EventList = ({ events, title }: Props) => {
+export const EventList = ({ events, emptyText, title }: Props) => {
   return (
     <>
       <Heading as="h2" marginBlock="1em" fontSize="lg">
         {title}
       </Heading>
-      <Grid gap="2em">
-        {events.map(({ canceled, id, invite_only, name }) => (
-          <GridItem key={id}>
-            <Flex justifyContent="space-between">
-              <LinkButton href={`/events/${id}`}>{name}</LinkButton>
-              <Flex>
-                {canceled && (
-                  <Tag
-                    borderRadius="lg"
-                    marginRight="3"
-                    paddingInline="[1 , 2]"
-                    paddingBlock="[.5, 1]"
-                    fontSize="['small', 'md']"
-                    maxWidth="8em"
-                    mt="1"
-                    maxH="2em"
-                    colorScheme="red"
-                  >
-                    Canceled
-                  </Tag>
-                )}
-                {invite_only && (
-                  <Tag
-                    borderRadius="lg"
-                    mt="1"
-                    paddingInline="[1 , 2]"
-                    paddingBlock="[.5, 1]"
-                    colorScheme="blue"
-                    fontSize="['small', 'md']"
-                    maxWidth="8em"
-                    maxH="2em"
-                  >
-                    Invite only
-                  </Tag>
-                )}
+      {events.length > 0 ? (
+        <Grid gap="2em">
+          {events.map(({ canceled, id, invite_only, name }) => (
+            <GridItem key={id}>
+              <Flex justifyContent="space-between">
+                <LinkButton href={`/events/${id}`}>{name}</LinkButton>
+                <Flex>
+                  {canceled && (
+                    <Tag
+                      borderRadius="lg"
+                      marginRight="3"
+                      paddingInline="[1 , 2]"
+                      paddingBlock="[.5, 1]"
+                      fontSize="['small', 'md']"
+                      maxWidth="8em"
+                      mt="1"
+                      maxH="2em"
+                      colorScheme="red"
+                    >
+                      Canceled
+                    </Tag>
+                  )}
+                  {invite_only && (
+                    <Tag
+                      borderRadius="lg"
+                      mt="1"
+                      paddingInline="[1 , 2]"
+                      paddingBlock="[.5, 1]"
+                      colorScheme="blue"
+                      fontSize="['small', 'md']"
+                      maxWidth="8em"
+                      maxH="2em"
+                    >
+                      Invite only
+                    </Tag>
+                  )}
+                </Flex>
               </Flex>
-            </Flex>
-          </GridItem>
-        ))}
-      </Grid>
+            </GridItem>
+          ))}
+        </Grid>
+      ) : (
+        <Heading as="h3" size="sm">
+          {emptyText || 'No events'}
+        </Heading>
+      )}
     </>
   );
 };

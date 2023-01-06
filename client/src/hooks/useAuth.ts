03d50d04254d37f1b8b35729163bc259423af758
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-01-06 12:01:19 +0100

    refactor: separate dev and auth0 logic

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -2,26 +2,35 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { useMeQuery } from 'generated/graphql';
 import { useSession } from 'hooks/useSession';
 
+const useDevLogin = () => {
+  return {
+    loginHelper: () => localStorage.setItem('dev-login-authenticated', 'true'),
+    logoutHelper: () => localStorage.removeItem('dev-login-authenticated'),
+  };
+};
+
+const useAuth0Login = () => {
+  const { loginWithPopup, logout } = useAuth0();
+  return {
+    loginHelper: loginWithPopup,
+    logoutHelper: logout,
+  };
+};
+
 export const useLogin = () => {
-  const { loginWithPopup, logout: logoutAuth0 } = useAuth0();
+  const { loginHelper, logoutHelper } =
+    process.env.NEXT_PUBLIC_USE_AUTH0 !== 'false'
+      ? useAuth0Login()
+      : useDevLogin();
+
   const { createSession, destroySession } = useSession();
   const { refetch } = useMeQuery();
-
-  const loginHelper =
-    process.env.NEXT_PUBLIC_USE_AUTH0 !== 'false'
-      ? loginWithPopup
-      : () => localStorage.setItem('dev-login-authenticated', 'true');
 
   const login = async () => {
     await loginHelper();
     await createSession();
     return await refetch();
   };
-
-  const logoutHelper =
-    process.env.NEXT_PUBLIC_USE_AUTH0 !== 'false'
-      ? logoutAuth0
-      : () => localStorage.removeItem('dev-login-authenticated');
 
   const logout = async () => {
     logoutHelper();

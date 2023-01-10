1f1c25b5c7dbd32b04f6aebd3fb9573db6595374
Sboonny <muhammed@freecodecamp.org>
2023-01-10 13:15:24 +0000

    feat: add logo to chapter card

--- client/src/components/ChapterCard.tsx
+++ client/src/components/ChapterCard.tsx
@@ -1,4 +1,4 @@
-import { Grid, Text, GridItem, Flex, Box } from '@chakra-ui/react';
+import { Grid, Text, GridItem, Flex, Box, Image } from '@chakra-ui/react';
 import { Link } from 'chakra-next-link';
 import React from 'react';
 
@@ -11,32 +11,45 @@ type ChapterCardProps = {
 
 export const ChapterCard: React.FC<ChapterCardProps> = ({ chapter }) => {
   return (
-    <Box
-      data-cy="chapter-card"
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-      width={'full'}
-      minW={'20em'}
-      gap={'2'}
-      backgroundImage={
-        chapter.banner_url ??
-        'https://cdn.freecodecamp.org/chapter/orange-graphics-small.jpg'
-      }
-      backgroundPosition={'center'}
-      backgroundRepeat={'no-repeat'}
-      backgroundSize={'cover'}
-    >
-      <Grid
-        templateColumns="repeat(3, 1fr)"
-        gap={'3'}
-        width="100%"
-        marginRight={'1em'}
-        paddingBlock={'.5em'}
-        color={'gray.00'}
-        bgGradient="linear(to-b,hsl(240 14% 27%/ .9),  hsl(240 14% 10%/ .9), hsl(240 14% 10%))"
-        alignItems="center"
-        templateAreas={`
+    <>
+      {chapter.logo_url && (
+        <Image
+          src={chapter.logo_url}
+          position="relative"
+          top="2.75em"
+          left="2em"
+          width="4.5em"
+          height="4.5em"
+          borderRadius="50%"
+          outline="2px solid white"
+        />
+      )}
+      <Box
+        data-cy="chapter-card"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        width={'full'}
+        minW={'20em'}
+        gap={'2'}
+        backgroundImage={
+          chapter.banner_url ??
+          'https://cdn.freecodecamp.org/chapter/orange-graphics-small.jpg'
+        }
+        backgroundPosition={'center'}
+        backgroundRepeat={'no-repeat'}
+        backgroundSize={'cover'}
+      >
+        <Grid
+          templateColumns="repeat(3, 1fr)"
+          gap={'3'}
+          width="100%"
+          marginRight={'1em'}
+          paddingBlock={'.5em'}
+          color={'gray.00'}
+          bgGradient="linear(to-b,hsl(240 14% 27%/ .9),  hsl(240 14% 10%/ .9), hsl(240 14% 10%))"
+          alignItems="center"
+          templateAreas={`
           ". . ."
           ". . ."
           "chaptername chaptername subnumber"
@@ -46,81 +59,84 @@ export const ChapterCard: React.FC<ChapterCardProps> = ({ chapter }) => {
           "event event event"
           "event event event"
           `}
-      >
-        <GridItem
-          paddingInline={'1em'}
-          paddingBlock={'.5em'}
-          area="chaptername"
         >
-          <Link
-            fontSize={['lg', 'xl', '2xl']}
-            fontWeight={700}
-            fontFamily={'body'}
-            paddingInline=".1em"
-            href={`/chapters/${chapter.id}`}
-            data-cy="chaptercard-name"
+          <GridItem
+            paddingInline={'1em'}
+            paddingBlock={'.5em'}
+            area="chaptername"
           >
-            {chapter.name}
-          </Link>
-        </GridItem>
-        <GridItem
-          display="inline-grid"
-          width="100%"
-          justifyItems="flex-end"
-          gridArea="subnumber"
-          paddingInline="1.5em"
-        >
-          <Text fontWeight="bold">Members: {chapter.chapter_users.length}</Text>
-        </GridItem>
-        <Text
-          paddingInline={'1em'}
-          fontSize={['md', 'lg', 'xl']}
-          fontWeight={'500'}
-          gridArea="aboutheader"
-        >
-          About
-        </Text>
-        <Text
-          noOfLines={3}
-          paddingInline={'1em'}
-          fontWeight={400}
-          fontSize={['sm', 'md', 'lg']}
-          gridArea="about"
-        >
-          {chapter.description}
-        </Text>
-        <Text
-          fontSize={['md', 'lg', 'xl']}
-          fontWeight={'500'}
-          paddingInline={'1em'}
-          marginBlockStart={'.5em'}
-          gridArea="eventheader"
-        >
-          Next Events
-        </Text>
-        <GridItem area="event" paddingInline={'1em'}>
-          {chapter.events.map(({ id, name, start_at }) => (
-            <Flex
-              paddingBlock={'.5em'}
-              paddingInline={'.3em'}
-              justifyContent={'space-between'}
-              key={id}
+            <Link
+              fontSize={['lg', 'xl', '2xl']}
+              fontWeight={700}
+              fontFamily={'body'}
+              paddingInline=".1em"
+              href={`/chapters/${chapter.id}`}
+              data-cy="chaptercard-name"
             >
-              <Link
-                href={`/events/${id}`}
-                mt="2"
-                fontWeight={600}
-                fontSize={['sm', 'md', 'lg']}
+              {chapter.name}
+            </Link>
+          </GridItem>
+          <GridItem
+            display="inline-grid"
+            width="100%"
+            justifyItems="flex-end"
+            gridArea="subnumber"
+            paddingInline="1.5em"
+          >
+            <Text fontWeight="bold">
+              Members: {chapter.chapter_users.length}
+            </Text>
+          </GridItem>
+          <Text
+            paddingInline={'1em'}
+            fontSize={['md', 'lg', 'xl']}
+            fontWeight={'500'}
+            gridArea="aboutheader"
+          >
+            About
+          </Text>
+          <Text
+            noOfLines={3}
+            paddingInline={'1em'}
+            fontWeight={400}
+            fontSize={['sm', 'md', 'lg']}
+            gridArea="about"
+          >
+            {chapter.description}
+          </Text>
+          <Text
+            fontSize={['md', 'lg', 'xl']}
+            fontWeight={'500'}
+            paddingInline={'1em'}
+            marginBlockStart={'.5em'}
+            gridArea="eventheader"
+          >
+            Next Events
+          </Text>
+          <GridItem area="event" paddingInline={'1em'}>
+            {chapter.events.map(({ id, name, start_at }) => (
+              <Flex
+                paddingBlock={'.5em'}
+                paddingInline={'.3em'}
+                justifyContent={'space-between'}
+                key={id}
               >
-                {name}
-              </Link>
-              <Text fontWeight={600} fontSize={['sm', 'md', 'lg']}>
-                {isPast(new Date(start_at)) ? 'Running' : 'Upcoming'}
-              </Text>
-            </Flex>
-          ))}
-        </GridItem>
-      </Grid>
-    </Box>
+                <Link
+                  href={`/events/${id}`}
+                  mt="2"
+                  fontWeight={600}
+                  fontSize={['sm', 'md', 'lg']}
+                >
+                  {name}
+                </Link>
+                <Text fontWeight={600} fontSize={['sm', 'md', 'lg']}>
+                  {isPast(new Date(start_at)) ? 'Running' : 'Upcoming'}
+                </Text>
+              </Flex>
+            ))}
+          </GridItem>
+        </Grid>
+      </Box>
+    </>
   );
 };

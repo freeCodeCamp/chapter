15af125f933d8e2a7424df93d95cb79c9865bd94
Sboonny <muhammed@freecodecamp.org>
2023-01-11 17:44:39 +0000

    feat: add freeCodeCamp tos and code of conduct

--- client/src/components/PageLayout/component/Footer.tsx
+++ client/src/components/PageLayout/component/Footer.tsx
@@ -1,16 +1,16 @@
-import { Link, Flex } from '@chakra-ui/react';
+import { Link, Grid } from '@chakra-ui/react';
 import React from 'react';
 
 export const Footer: React.FC = () => {
   return (
-    <Flex
+    <Grid
       as={'footer'}
       marginTop="2em"
       backgroundColor={'gray.10'}
       paddingInline="2em"
       paddingBlock="1em"
-      gap="2em"
-      justifyContent="space-evenly"
+      gap="3em"
+      gridTemplateColumns="repeat(auto-fit, minmax(8rem, 1fr))"
     >
       <Link href="/chapters" fontWeight="600">
         Chapters
@@ -18,8 +18,19 @@ export const Footer: React.FC = () => {
       <Link href="/events" fontWeight="600">
         Events
       </Link>
-      <Link href="/policy" fontWeight="600">
+      <Link
+        href="https://www.freecodecamp.org/news/privacy-policy/"
+        fontWeight="600"
+        isExternal
+      >
         Policy
+      </Link>
+      <Link
+        href="https://www.freecodecamp.org/news/terms-of-service/"
+        fontWeight="600"
+        isExternal
+      >
+        Terms Of Service
       </Link>
       <Link
         fontWeight="600"
@@ -28,6 +39,6 @@ export const Footer: React.FC = () => {
       >
         Code Of Conduct
       </Link>
-    </Flex>
+    </Grid>
   );
 };

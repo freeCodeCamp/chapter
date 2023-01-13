ecb67275c467434f85918ad74fc82ea552f86ca7
Sboonny <muhammed@freecodecamp.org>
2023-01-13 07:39:49 +0000

    remove chapter context

--- client/src/modules/chapters/chapterContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import { useChapterQuery } from '../../generated/graphql';
-
-const { data } = useChapterQuery({});
-
-export const ChapterCardData = createContext(data);

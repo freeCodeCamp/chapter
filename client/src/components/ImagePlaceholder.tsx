c22908d53c9a279afe4b84cc72545b267ad383f5
Aashutosh Poudel <aashutoshpoudyal@gmail.com>
2023-02-03 02:48:10 +0545

    fix: refactor and fix failing test

--- client/src/components/ImagePlaceholder.tsx
+++ client/src/components/ImagePlaceholder.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from '@chakra-ui/react';
 
-export default function DefaultImage() {
+export default function ImagePlaceholder() {
   return (
     <Image src="/freecodecamp-logo.svg" background="gray.85" height="100%" />
   );

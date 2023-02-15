4014640c439efaabb4ba42df501feb2410ad69e5
Krzysztof G <60067306+gikf@users.noreply.github.com>
2023-02-15 07:35:44 +0100

    feat: add specific success text when placed on waitlist (#2381)

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -1669,7 +1669,11 @@ export type AttendEventMutationVariables = Exact<{
 
 export type AttendEventMutation = {
   __typename?: 'Mutation';
-  attendEvent: { __typename?: 'EventUserWithRelations'; updated_at: any };
+  attendEvent: {
+    __typename?: 'EventUserWithRelations';
+    updated_at: any;
+    attendance: { __typename?: 'Attendance'; name: string };
+  };
 };
 
 export type CancelAttendanceMutationVariables = Exact<{
@@ -4795,6 +4799,9 @@ export const AttendEventDocument = gql`
   mutation attendEvent($eventId: Int!, $chapterId: Int!) {
     attendEvent(eventId: $eventId, chapterId: $chapterId) {
       updated_at
+      attendance {
+        name
+      }
     }
   }
 `;

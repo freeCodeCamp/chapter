ac45231d6046232ff8cf15d026391086554490ae
Sboonny <88248797+Sboonny@users.noreply.github.com>
2023-02-22 15:55:42 +0200

    create setting page

---
+++ client/src/modules/dashboard/Settings/pages/Settings.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Settings = () => {
+  return <h1>Settings</h1>;
+};
---
+++ client/src/pages/dashboard/settings/index.tsx
@@ -0,0 +1,2 @@
+import { Settings } from '../../../modules/dashboard/Settings';
+export default Settings;

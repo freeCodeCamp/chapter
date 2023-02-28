355316bacabe48063ba38a7bf43bc51ffd5766c5
Sboonny <88248797+Sboonny@users.noreply.github.com>
2023-02-28 20:24:33 +0200

    missing useupdateInstanceMutation

--- client/src/modules/dashboard/InstanceSettings/pages/InstanceSettings.tsx
+++ client/src/modules/dashboard/InstanceSettings/pages/InstanceSettings.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Form } from '../../../../components/Form/Form';
+import { useDisableWhileSubmitting } from '../../../../hooks/useDisableWhileSubmitting';
+import { UpdateInstanceSettingsInputs } from '../../../../generated/graphql';
 
 export const InstanceSettings = () => {
-  return <h1>InstanceSettings</h1>;
+  const { handleSubmit } = useForm();
+  const [updateInstanceSettings] = useinstanceMutation();
+  const onSubmit = async (data: UpdateInstanceSettingsInputs) => {
+    const { errors } = await updateInstanceSettings({
+      variables: { data: data },
+    });
+    if (errors) throw errors;
+  };
+
+  const { disableWhileSubmitting } = useDisableWhileSubmitting({
+    onSubmit,
+  });
+  return (
+    <>
+      <h1>InstanceSettings</h1>
+      <Form
+        submitLabel="Edit the instance settings"
+        FormHandling={handleSubmit(disableWhileSubmitting)}
+      ></Form>
+    </>
+  );
 };

9ed89017c257a1724c6491d82b0703d806e4fc9b
gikf <60067306+gikf@users.noreply.github.com>
2023-03-28 20:44:51 +0200

    refactor: extract event data

--- client/tests/EventCard.test.tsx
+++ client/tests/EventCard.test.tsx
@@ -6,24 +6,22 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { EventCard } from '../src/components/EventCard';
 
+const event = {
+  chapter: { id: 1, name: 'foo' },
+  id: 1,
+  name: 'bar',
+  description: 'baz',
+  start_at: 123,
+  ends_at: 123,
+  invite_only: true,
+  canceled: false,
+  image_url: 'http://example.com/image.png',
+  event_tags: [],
+};
+
 describe('EventCard', () => {
   it('should render', () => {
-    const { container } = render(
-      <EventCard
-        event={{
-          chapter: { id: 1, name: 'foo' },
-          id: 1,
-          name: 'bar',
-          description: 'baz',
-          start_at: 123,
-          ends_at: 123,
-          invite_only: true,
-          canceled: false,
-          image_url: 'http://example.com/image.png',
-          event_tags: [],
-        }}
-      />,
-    );
+    const { container } = render(<EventCard event={event} />);
     expect(container).toMatchSnapshot();
   });
 
@@ -31,15 +29,7 @@ describe('EventCard', () => {
     const { container } = render(
       <EventCard
         event={{
-          chapter: { id: 1, name: 'foo' },
-          id: 1,
-          name: 'bar',
-          description: 'baz',
-          start_at: 123,
-          ends_at: 123,
-          invite_only: true,
-          canceled: false,
-          image_url: 'http://example.com/image.png',
+          ...event,
           event_tags: [
             { tag: { id: 1, name: 'notfoo' } },
             { tag: { id: 2, name: 'notbar' } },
